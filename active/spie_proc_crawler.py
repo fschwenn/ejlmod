a4e1d19ddc13d8b5589a181d97a6ea8f8d407f7f
fschwenn <florian.schwennsen@desy.de>
2017-08-30 12:32:49 +0200

    spie_proc_crawler: moved to right directory

--- active/spie_proc_crawler.py
+++ active/spie_proc_crawler.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+#program to harvest SPIE Proceedings
+# FS 2017-08-27
 
 import getopt
 import sys
@@ -19,105 +21,87 @@ xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
 
 
 
-def spie(volumeid):
+def spie(volume):
     jnlname = 'Proc.SPIE Int.Soc.Opt.Eng.'
     #jnlname = 'Proc.SPIE'
-    urltrunc = "http://proceedings.spiedigitallibrary.org/"
-    toclink = "volume.aspx?volumeid=%s" % (volumeid)
-    print 'open %s%s' % (urltrunc, toclink)
-    page = BeautifulSoup(urllib2.urlopen(urltrunc + toclink))
-    #except:
-    #    urltrunc = 'http://spie.org/Publications/Proceedings/'
-    #    toclink = 'Volume/%s' % (volumeid)
-    #    print 'open %s%s' % (urltrunc, toclink)
-    #    page = BeautifulSoup(urllib2.urlopen(urltrunc + toclink))
-    pageheader = page.body.find('div', attrs = {'class' : 'pageHeader'})
-    spans = pageheader.find_all('span')
-    volume = re.sub('Volume ','',spans[0].string.strip())
-    conftitle = spans[1].string.strip()
-    editors = pageheader.find('div', attrs = {'class' : 'authors'}).string.strip()
-    recs = [{'tit' : conftitle, 'jnl' : jnlname, 'vol' : volume, 'tc' : 'K'}]
-    recs[0]['auts'] = [ejlmod2.shapeaut(editor)+' (Ed.)' for editor in re.split('; ',editors)]
-    print jnlname,volume
+    urltrunc = "https://www.spiedigitallibrary.org"
+    toclink = "%s/conference-proceedings-of-spie/%s.toc" % (urltrunc, volume)
+    print 'open %s' % (toclink)
+    page = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(toclink))
+    for text in page.body.find_all('text', attrs = {'class' : 'ProceedingsArticleVolTitleText'}):
+        conftitle = text.text.strip()
+    (section, articletype) = ('', '')
+    recs = []
     for div in page.body.find_all('div'):
         if div.attrs.has_key('class'):
-            if 'articleType' in div['class']:
+            if 'TOCLineItemSectionHeaderDiv' in div['class']:
+                section = div.text.strip()
+            elif 'articleType' in div['class']:
                 articletype = div.find('a').string
-            elif 'articleContent' in div['class'] and not re.search('^Front Matter', articletype):
-                rec = {'keyw' : [], 'note' : [articletype], 'jnl' : jnlname, 'vol' : volume, 'auts' : [], 'tc' : 'C', 'aff' : [], 'refs' : []}
-                tempauts = []
-                link = div.find('a', attrs = {'class' : 'relatedArticle'})['href']
-                print '  open %s%s' % (urltrunc, link)
-                time.sleep(10)
-                try:
-                    articlepage = BeautifulSoup(urllib2.urlopen(urltrunc +link, timeout=300))
-                except:
-                    print 'retry %s in 5 minutes' % (urltrunc +link)
-                    time.sleep(300)
-                    try:
-                        articlepage = BeautifulSoup(urllib2.urlopen(urltrunc +link, timeout=300))
-                    except:
-                        print 'could not get metadata for %s' % (link)
-                        continue
-                for abstract in articlepage.body.find_all('span', attrs = {'class' : 'Abstract'}):
-                    try:
-                        rec['abs'] = abstract.string
-                    except:
-                        for para in abstract.find_all('p', attrs = {'class' : 'para'}):
-                            rec['abs'] = para.string
-                (affsofaut, autsofaff) = ({}, {})
-                for meta in articlepage.head.find_all('meta'):
-                    if 'citation_title' in meta['name']:
-                        rec['tit'] = meta['content']
-                    elif 'citation_firstpage' in meta['name']:
-                        rec['p1'] = meta['content']
-                    elif 'citation_lastpage' in meta['name']:
-                        #rec['p2'] = meta['content']
-                        rec['pages'] = re.sub('.*\-','',meta['content'])
-                    elif 'citation_doi' in meta['name']:
-                        rec['doi'] = meta['content']
-                    elif 'citation_keyword' in meta['name']:
-                        rec['keyw'].append(meta['content'])
-                    elif 'citation_publication_date' in meta['name']:
-                        rec['date'] = meta['content']
-                        rec['year'] = rec['date'][:4]
-                    elif 'pdf' in meta['name']:
-                        for OA in div.find_all('img', attrs = {'src' : 'Images/icons/icon_openaccess.png'}):
-                            rec['pdf'] = meta['content']
-                    elif 'citation_author_institution' in meta['name']:
-                        affsofaut[tempauts[-1]].append(meta['content'])
-                        if autsofaff.has_key(meta['content']):
-                            autsofaff[meta['content']][0].append(tempauts[-1])
-                        else:
-                            autsofaff[meta['content']] = [[tempauts[-1]],len(autsofaff)+1]
-                    elif 'citation_author' in meta['name']:
-                        tempauts.append(meta['content'])
-                        affsofaut[tempauts[-1]] = []
-                #for div in articlepage.find_all('div', attrs = {'class' : 'refContent'}):
-                #    rec['refs'].append([('x', div.text)])
-                if len(args) > 1:
-                    rec['cnum'] = args[1]
-
-                #print autsofaff
-                #print affsofaut
-
-                for aut in tempauts:
-                    rec['auts'].append(ejlmod2.shapeaut(aut))
-                    for  aff in affsofaut[aut]:
-                        rec['auts'].append('=Aff%s' % autsofaff[aff][1])
-                for tupel in sorted([(int(autsofaff[aff][1]),aff) for aff in autsofaff]):
-                    rec['aff'].append('Aff%i= %s' % (tupel[0],tupel[1]))
-                if rec.has_key('year'):
-                    print '%s %s (%s) %s, %s' % (jnlname,volume,rec['year'],rec['p1'],rec['tit'])
+            elif 'TOCLineItemRow1' in div['class'] and not re.search('^Front Matter', section):
+                rec = {'keyw' : [], 'note' : [section, articletype], 'jnl' : jnlname, 'vol' : volume, 
+                       'tc' : 'C', 'autaff' : [], 'refs' : []}
+                for a in div.find_all('a', attrs = {'class' : 'TocLineItemAnchorText1'}):
+                    rec['artlink'] = '%s%s' % (urltrunc, a['href'])
+                    rec['tit'] = a.text.strip()
                     recs.append(rec)
-                else:
-                    print '=== PROBLEM WITH RECORD ==='
-                    print rec
-                    print '==========================='
-
-    recs[0]['year'] = rec['year']
-    if len(args) > 2:
-        recs[0]['cnum'] = args[2]
+    #get detailed informations from article pages
+    i = 0
+    for rec in recs:
+        i += 1
+        print '  open [%i/%i] %s' % (i, len(recs), rec['artlink'])
+        try:
+            time.sleep(20)
+            articlepage = BeautifulSoup(urllib2.urlopen(rec['artlink'], timeout=300))
+        except:
+            print 'retry %s in 5 minutes' % (urltrunc +link)
+            time.sleep(300)
+        autaff = []
+        for meta in articlepage.head.find_all('meta'):
+            if meta.has_attr('name'):
+                if 'citation_firstpage' in meta['name']:
+                    rec['p1'] = meta['content']
+                elif 'citation_lastpage' in meta['name']:
+                    rec['pages'] = re.sub('.*\-','',meta['content'])
+                elif 'citation_doi' in meta['name']:
+                    rec['doi'] = meta['content']
+                elif 'citation_abstract' in meta['name']:
+                    rec['abs'] = meta['content']
+                elif 'citation_keyword' in meta['name']:
+                    for keyw in re.split('; ', meta['content']):
+                        if keyw: rec['keyw'].append(keyw)
+                elif 'citation_publication_date' in meta['name']:
+                    rec['date'] = meta['content']
+                    rec['year'] = rec['date'][:4]
+                elif 'pdf' in meta['name']:
+                    for OA in articlepage.body.find_all('img', attrs = {'src' : "/Content/themes/SPIEImages/OpenAccessIcon.png"}):
+                        rec['FFT'] = meta['content']
+                elif meta['name'] == 'citation_author':
+                    if autaff:
+                        rec['autaff'].append(autaff)
+                    autaff = [ meta['content'] ]
+                elif meta['name'] == 'citation_author_institution':
+                    autaff.append(meta['content'])
+                elif meta['name'] == 'citation_author_orcid':
+                    orcid = re.sub('.*\/', '', meta['content'])
+                    autaff.append('ORCID:%s' % (orcid))
+                elif meta['name'] == 'citation_author_email':
+                    email = meta['content']
+                    autaff.append('EMAIL:%s' % (email))   
+        rec['autaff'].append(autaff)
+        #number of pages
+        for div in articlepage.body.find_all('text', attrs = {'class' : 'ProceedingsArticleSmallFont'}):
+            pages = re.split(' *', div.text.strip())
+            if len(pages) == 2 and pages[1] in ['pages', 'PAGES']:
+                rec['pages'] = pages[0]
+        if len(args) > 1:
+            rec['cnum'] = args[1]
+        if rec.has_key('year'):
+            print '  %s %s (%s) %s, %s' % (jnlname,volume,rec['year'],rec['p1'],rec['tit'])
+        else:
+            print '=== PROBLEM WITH RECORD ==='
+            print rec
+            print '==========================='
     return recs
 
 
@@ -125,27 +109,26 @@ def spie(volumeid):
 
 if __name__ == '__main__':
     usage = """
-        python spie_proc_crawler.py volumeid [cnum]
+        python spie_proc_crawler.py volume [cnum]
     """
     try:
         opts, args = getopt.getopt(sys.argv[1:], "")
         if len(args) > 2:
             raise getopt.GetoptError("Too many arguments given!!!")
         elif not args:
-            raise getopt.GetoptError("Missing mandatory argument volumeid")
+            raise getopt.GetoptError("Missing mandatory argument volume")
     except getopt.GetoptError as err:
         print(str(err))  # will print something like "option -a not recognized"
         print(usage)
         sys.exit(2)
-    volumeid = args[0]
+    volume = args[0]
     publisher = 'International Society for Optics and Photonics'
-    recs = spie(volumeid)
-    rvolumeid = recs[0]['vol']
+    recs = spie(volume)
     if len(args) > 1:
         cnum = args[1]
-        outfile = 'spie%s_%s.xml' % (rvolumeid, cnum)
+        outfile = 'spie%s_%s.xml' % (volume, cnum)
     else:
-        outfile = 'spie%s.xml' % (rvolumeid)
+        outfile = 'spie%s.xml' % (volume)
     #dokf = codecs.EncodedFile(open(os.path.join(xmldir,outfile),mode='wb'),"utf8")
     dokf = open(os.path.join(xmldir,outfile),'w')
     ejlmod2.writeXML(recs,dokf,publisher)
