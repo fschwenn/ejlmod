c5b7a14e3a0c47420bc02e4043d7a91e46743f6b
fschwenn <florian.schwennsen@desy.de>
2017-11-13 16:00:15 +0100

    cambridge: avoid back- and front-matter

--- active/cambridge.py
+++ active/cambridge.py
@@ -8,7 +8,7 @@ import sys
 import os
 import ejlmod2
 import re
-import urllib2
+import urllib2,cookielib
 import urlparse
 import codecs
 import time
@@ -21,6 +21,7 @@ def tfstrip(x): return x.strip()
 
 publisher = 'Cambridge University Press'
 
+tc = 'P'
 jid =  sys.argv[1]
 vol = sys.argv[2]
 jnlfilename = 'cambridge'+jid + vol
@@ -30,13 +31,16 @@ if len(sys.argv) > 3:
 
 if len(sys.argv) > 4:
     jnlfilename += '.' + sys.argv[4]
+    tc = 'C'
+if len(sys.argv) > 5:
+    explicittoclink = sys.argv[5]
 
 if jid == 'IAU':
     jnlname = 'IAU Symp.'
 elif jid == 'PSP':
     jnlname = 'Math.Proc.Cambridge Phil.Soc.'
 elif jid == 'FMS':
-    jnlname = 'SIGMA'
+    jnlname = 'Forum Math.Sigma'
 elif jid == 'LPB':
     jnlname = 'Laser Part.Beams'
 elif jid == 'JAZ':
@@ -49,172 +53,170 @@ elif jid == 'IJA':
     jnlname = 'Int.J.Astrobiol.'
 elif jid == 'GMJ':
     jnlname = 'Glasgow Math.J.'
+#--
+elif jid == 'BAZ':
+    jnlname = 'Bull.Austral.Math.Soc.'
+elif jid == 'CPH':
+    jnlname = 'Commun.Comput.Phys.'
+elif jid == 'COM':
+    jnlname = 'Compos.Math.'
+elif jid == 'FMP':
+    jnlname = 'Forum Math.Pi'
+elif jid == 'MTK':
+    jnlname = 'Mathematika'
 
-
-toclink = 'http://journals.cambridge.org/action/displayIssue?jid=%s&volumeId=%s' % (jid, vol)
-if len(sys.argv) > 3:
-    toclink += '&issueId=%s' % (iss)
-    print toclink
+if len(sys.argv) > 5:
+    toclink = explicittoclink
+else:
+    toclink = 'http://journals.cambridge.org/action/displayIssue?jid=%s&volumeId=%s' % (jid, vol)
+    if len(sys.argv) > 3:
+        toclink += '&issueId=%s' % (iss)
+        print toclink
         
-try:
-    toc = BeautifulSoup(urllib2.urlopen(toclink, timeout=300))
-except:
-    print "retry in 180 seconds"
-    time.sleep(180)
-    toc = BeautifulSoup(urllib2.urlopen(toclink, timeout=300))
+if not os.path.isfile('/tmp/%s.0.toc' % (jnlfilename)):
+    os.system('wget -O /tmp/%s.0.toc "%s"' % (jnlfilename, toclink))
+tocf = open('/tmp/%s.0.toc' % (jnlfilename), 'r')
+toc = BeautifulSoup(''.join(tocf.readlines()))
+tocf.close()
 
+#check number of toc-pages
+for meta in toc.head.find_all('meta', attrs = {'property' : 'og:url'}):
+    baseurl = 'https://www.cambridge.org' + meta['content']
+    numpages = 1
+    for div in toc.body.find_all('div', attrs = {'class' : 'results'}):
+        for p in div.find_all('p', attrs = {'class' : 'paragraph_05'}):
+            ptext = p.text.strip()
+            if re.search('age \d+ of \d+', ptext):
+                numpages = int(re.sub('.* of (\d+).*', r'\1', ptext))
+
+note = ''
 recs = []
-for div in toc.body.find_all('div', attrs = {'class' : 'tableofcontents'}):
-    for p in div.find_all('p'):
-        note = re.sub('\xa0', ' ', re.sub('[\t\n\r]', ' ', p.text))
-        note = re.sub('Table of Contents * \-', '', note)
-        note = re.sub('Volume *\w* *\-', '', note)
-        note = re.sub('Issue *\w* *\-', '', note)
-        note = re.sub('  +', '', note).strip()
+#first run through TOC to get DOIs
+for i in range(numpages):
+    toclink = '%s?pageNum=%i' % (baseurl, i+1)
+    if not os.path.isfile('/tmp/%s.%i.toc' % (jnlfilename, i)):
+        os.system('wget -O /tmp/%s.%i.toc "%s"' % (jnlfilename, i, toclink))
+    tocf = open('/tmp/%s.%i.toc' % (jnlfilename, i), 'r')
+    toc = BeautifulSoup(''.join(tocf.readlines()))
+    tocf.close()
+    for div in toc.body.find_all('div'):
+        if div.has_attr('class') and 'columns' in div['class']:
+            if 'large-12' in div['class'] and 'margin-top' in div['class']:
+                for child in div.children:
+                    try:
+                        child.name
+                    except:
+                        continue
+                    if child.name == 'h4':
+                        note = child.text.strip()
+                    elif child.name == 'div':
+                        for a in child.find_all('a', attrs = {'class' : 'url doi'}):
+                            rec = {'artlink' : a['href'], 'refs' : [], 'tc' : tc,
+                                   'autaff' : [], 'keyw' : [], 'jnl' : jnlname}
+                            rec['note'] = [ note ]
+                            rec['doi'] = re.sub('.*doi.org.', '', a['href'])
+                            if not note in ['Front Cover (OFC, IFC) and matter', 
+                                        'Back Cover (OBC, IBC) and matter']:
+                                recs.append(rec)
+                                print rec['doi'], rec['note']
 
-for a in toc.body.find_all('a', attrs = {'title' : 'Abstract'}):
-    rec = {'jnl' : jnlname, 'keyw' : [], 'note' : [note]}
+
+#2nd run to get details for individual articles
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+i = 0
+for rec in recs:    
+    i += 1 
+    req = urllib2.Request(rec['artlink'], headers=hdr)
+    try:
+        artpage = BeautifulSoup(urllib2.urlopen(req))
+        time.sleep(10)
+    except:
+        print 'wait 3 minutes'
+        time.sleep(180)
+        artpage = BeautifulSoup(urllib2.urlopen(req))
+        time.sleep(2)
+    print '------{ %s }------{ %i/%i }------' % (rec['doi'], i, len(recs))
+    for meta in artpage.head.find_all('meta'):
+        if meta.attrs.has_key('name'):
+            #title
+            if meta['name'] == 'citation_title':
+                rec['tit'] = meta['content']
+            #pubnote
+            elif meta['name'] == 'citation_firstpage':
+                rec['p1'] = meta['content']
+            elif meta['name'] == 'citation_lastpage':
+                rec['p2'] = meta['content']
+            elif meta['name'] == 'citation_volume':
+                if jid == 'IAU':
+                    rec['vol'] = iss[1:]
+                else:
+                    rec['vol'] = meta['content']
+            elif meta['name'] == 'citation_issue':
+                if not jid == 'IAU':
+                    rec['issue'] = meta['content']
+            elif meta['name'] == 'citation_publication_date':
+                rec['year'] = meta['content'][:4]
+            #authors
+            elif meta['name'] == 'citation_author':
+                rec['autaff'].append([meta['content'].title()])
+            elif meta['name'] == 'citation_author_institution':
+                rec['autaff'][-1].append(meta['content'])
+            elif meta['name'] == 'citation_author_email':
+                email = meta['content']
+                rec['autaff'][-1].append('EMAIL:%s' % (email))
+            elif meta['name'] == 'citation_author_orcid':
+                orcid = re.sub('.*\/', '', meta['content'])
+                rec['autaff'][-1].append('ORCID:%s' % (orcid))
+            #date
+            elif meta['name'] == 'citation_online_date':
+                 rec['date'] = meta['content']
+            #keywords
+            elif meta['name'] == 'citation_keywords':
+                 rec['keyw'] += re.split(' *; *', meta['content'])
+    #CNUM
     if len(sys.argv) > 4:
         rec['cnum'] = sys.argv[4]
-    if jid in ['IAU']: 
-        rec['tc'] = 'C'
-        rec['vol'] = iss[1:]
-        if rec['vol'] == '308':
-            rec['cnum'] = 'C14-06-23.11'
-        elif rec['vol'] == '306':
-            rec['cnum'] = 'C14-05-25.1'
-        elif rec['vol'] == '313':
-            rec['cnum'] = 'C14-09-15.13'
-        elif rec['vol'] == '312':
-            rec['cnum'] = 'C14-08-25.9'
-    else:
-        rec['tc'] = 'P'
-        rec['vol'] = vol
-        if len(sys.argv) > 3:
-            rec['issue'] = iss
-    artlink = 'http://journals.cambridge.org/action/' + a['href']
-    try:
-        #print 'artlink', artlink
-        art = BeautifulSoup(urllib2.urlopen(artlink, timeout=300))
-        time.sleep(2)
-    except:
-        print "retry in 180 seconds"
-        time.sleep(180)
-        art = BeautifulSoup(urllib2.urlopen(artlink, timeout=300))
-    #OA
-    for ul in art.body.find_all('ul', attrs = {'class' : 'jnlDetails'}):
-        for li in ul.find_all('li'):
-            if re.search('creativecommons', li.text):
-                rec['licence'] = {'url' : re.sub('.*(http:\/\/creativecommons.org.*?0).*', r'\1', li.text).strip()}
-    #Title, Abstract
-    for meta in art.head.find_all('meta'):
-        if meta.has_attr('property'):
-            if meta['property'] == 'og:title':
-                rec['tit'] = meta['content']
-            if meta['property'] == 'og:description':
-                rec['abs'] = meta['content']
-    #Date, Pages, Keywords
-    for meta in art.body.find_all('meta'):
-        if meta.has_attr('name'):
-            if meta['name'] == 'citation_online_date':
-                rec['date'] = meta['content']
-            if meta['name'] == 'citation_firstpage':
-                rec['p1'] = meta['content']
-            if meta['name'] == 'citation_lastpage':
-                if meta['content'] != '':
-                    rec['p2'] = meta['content']
-                else:
-                    for ul in art.body.find_all('ul', attrs = {'class' : 'jnlDetails'}):
-                        for li in ul.find_all('li'):
-                            litext = li.text
-                            if re.search('Volume.*page', litext):
-                                rec['p1'] = re.sub('.*, (\d+) \(\d+ page.*', r'\1', litext)
-                                rec['pages'] = re.sub('.*, \d+ \((\d+) page.*', r'\1', litext)                    
-                                break
-            if meta['name'] == 'citation_keywords':
-                if re.search(ur'\u2014', meta['content']):
-                    for kw in re.split(ur'\u2014', meta['content']): 
-                       rec['keyw'].append(kw.strip())
-                else:
-                    for kw in re.split(';',  meta['content']):
-                        rec['keyw'].append(kw.strip())
-    #DOI
-    for a in art.body.find_all('a', attrs = {'class' : 'cboDOI'}):
-        rec['doi'] = re.sub('.*dx.doi.org.', '', a.string).strip()
-        print rec['doi']
-    #Authors
-    descriptionboxs  = art.body.find_all('div', attrs = {'class' : 'description-box'})
-    if len(descriptionboxs) == 0:
-        print 'NO AUTHORS???'
-        rec['auts'] = ['John Doe']
-    elif len(descriptionboxs[0].find_all('h3', attrs = {'class' : 'author'})) == 0:
-        print 'NO AUTHORS??'
-        rec['auts'] = ['John Doe']        
-    else:
-        for h3 in descriptionboxs[0].find_all('h3', attrs = {'class' : 'author'}):
-            for sup in h3.find_all('sup'):
-                try:
-                    if re.search('[a-z0-9]', sup.a.string):
-                        cont = re.sub('^[a-z]*', '', sup.a.string)
-                        sup.replace_with(', =Aff%s' % (cont))
-                    else:
-                        sup.replace_with('')
-                except:
-                    try:
-                        cont = re.sub('^[a-z]*', '', sup.string)
-                        if re.search('[a-z0-9]', cont):
-                            sup.replace_with(', =Aff%s' % (cont))
-                        else:
-                            sup.replace_with('')
-                    except:
-                        print 'author problem??', h3
-        rec['auts'] = re.split(', ', re.sub(' and ', ', ', re.sub('\xa0', ' ', h3.text)))
-        #Affiliations
-        for p in descriptionboxs[0].find_all('p'):
-            for sup in p.find_all('sup'):
-                try:
-                    cont = re.sub('^[a-z]*', '', sup.string.strip())
-                    sup.replace_with('XXX Aff%s= ' % (cont))
-                except:
-                    pass
-            if p.has_attr('class') and 'smallcopy' in p['class']:
-                p.replace_with('DIRTYSTART')
-            elif p.has_attr('class') and 'section-title' in  p['class']:
-                p.replace_with('DIRTYSTOP')
-        affstring = re.sub(' *\n *', ' ', descriptionboxs[0].text)
-        affstring = re.sub('.*DIRTYSTART(.*)DIRTYSTOP.*', r'\1', affstring).strip()
-        affstring = re.sub('  +' , ' ', affstring)
-        rec['aff'] = re.split(' *XXX *', affstring)
-    #References
-    for a in art.body.find_all('a', attrs = {'class' : 'article-ref'}):
-    #if 1==0:
-        reflink = 'http://journals.cambridge.org/action/' + a['href']
-        reflink = re.sub('[\r\t\n ]', '', reflink)
-        try:
-            #print 'reflink', reflink
-            ref = BeautifulSoup(urllib2.urlopen(reflink))
-        except:
-            print "retry in 180 seconds"
-            time.sleep(180)
-            ref = BeautifulSoup(urllib2.urlopen(reflink))
-        rec['refs'] = []
-        fulltxt = ref.body.find_all('div', attrs = {'class' : 'fulltxt'})
-        if fulltxt:
-            for li in fulltxt[0].find_all('li'):
-                if li.has_attr('id'):
-                    ref = [('o',  re.sub('ref', '', li['id']))]
-                    for a in li.find_all('a'):
-                        if a.has_attr('href') and re.search('dx.doi.org', a['href']):
-                            ref.append(('a', re.sub('.*dx.doi.org.', '', a['href'])))
-                    if len(ref) == 1:      
-                        ref = [('x', li.text)]
-                    rec['refs'].append(ref)
-        else:
-            print 'no references (?)'
-    recs.append(rec)
-
-
-
+    #articleID
+    if not rec.has_key('p1'):
+        for ul in artpage.body.find_all('ul', attrs = {'class' : 'title-volume-issue'}):
+            for li in ul.find_all('li', attrs = {'class' : 'published'}):
+                rec['p1'] = re.sub('.*, ', '', re.sub('\n', ' ', li.text.strip()))
+    #abstract
+    for div in artpage.body.find_all('div', attrs = {'class' : 'abstract'}):
+        for tit in div.find_all('title'):
+            tit.replace_with('')
+        rec['abs'] = div.text.strip()
+    #references (only with DOI)
+    for div in artpage.body.find_all('div', attrs = {'id' : 'references'}):
+        for child in div.children:
+            try:
+                child.name
+            except:
+                continue
+            if child.name == 'div':
+                reference = child.text.strip()
+            elif child.name == 'ul':
+                for a in child.find_all('a'):
+                    if a.text == 'CrossRef':
+                        refdoi = re.sub('.*doi.org.', '', a['href'])
+                        reference += ', DOI: ' + refdoi
+            elif child.name == 'hr':
+                rec['refs'].append([('x', reference)])                
+    #licence
+    for div in artpage.body.find_all('div', attrs = {'class' : 'description'}):
+        for div2 in div.find_all('div', attrs = {'class' : 'margin-top'}):
+            div2text = div2.text.strip()
+            if re.search('creativecommons.org', div2text):
+                rec['licence'] = {'url' : re.sub('.*(http.*?creativecommons.*?0).*', r'\1', div2text)}
+                for meta in artpage.head.find_all('meta', attrs = {'name' : 'citation_pdf_url'}):
+                    rec['FFT'] = meta['content']
+    #print rec
+        
 
 
 
