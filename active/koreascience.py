b0666d222834d03adb3c33d963a40ab69ea7bf60
Florian Schwennsen <florian.schwennsen@desy.de>
2019-04-11 14:51:22 +0200

    koreascience: changes in web page

--- active/koreascience.py
+++ active/koreascience.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 #!/usr/bin/python
 #program to harvest Korea Science
-# FS 2015-11-13
+# FS 2019-04-03
 
 import os
 import ejlmod2
@@ -14,6 +14,7 @@ import urllib2
 import urlparse
 from bs4 import BeautifulSoup
 import datetime
+import time
 
 xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
 ejldir = '/afs/desy.de/user/l/library/dok/ejl'
@@ -26,12 +27,16 @@ journals = {'jkas' : {'issn'      : '1225-4614',
             'jass' : {'issn'      : '2093-5587',
                       'publisher' : 'The Korean Space Science Society',
                       'jnl'       : 'J.Astron.Space Sci.',
-                      'kojic'     : 'OJOOBS'},
+                      'kojic'     : 'OJOOBS',
+                      'licence'   : {'statement' : 'CC-BY-NC-3.0'}},
             'jkms' : {'issn'      : '0304-9914',
                       'publisher' : 'The Korean Mathematical Society',
                       'jnl'       : 'J.Korean Math.Soc.',
                       'kojic'     : 'DBSHBB'}}
 
+
+
+        
 journal = sys.argv[1]
 vol = sys.argv[2]
 iss = sys.argv[3]
@@ -42,28 +47,39 @@ publisher = journals[journal]['publisher']
 
 jnlfilename = '%s%s.%s' % (journal, vol, iss)
 
-starturl = 'http://koreascience.or.kr/journal/JournalIssueList.jsp?kojic=%s&volno=%s&issno=%s' % (journals[journal]['kojic'], vol, iss)
+#starturl = 'http://koreascience.or.kr/journal/JournalIssueList.jsp?kojic=%s&volno=%s&issno=%s' % (journals[journal]['kojic'], vol, iss)
+starturl = 'http://koreascience.or.kr/journal/%s/v%sn%s.page' % (journals[journal]['kojic'], vol, iss)
 print starturl
 #tocpage = BeautifulSoup(urllib2.urlopen(starturl, timeout=300))
-os.system('lynx -source "%s" > /tmp/%s.toc' % (starturl, jnlfilename))
+if not os.path.isfile('/tmp/%s.toc' % (jnlfilename)):
+    os.system('lynx -source "%s" > /tmp/%s.toc' % (starturl, jnlfilename))
 tocf = open('/tmp/%s.toc' % (jnlfilename), 'r')
 tocpage = BeautifulSoup(''.join(tocf.readlines()))
 tocf.close()
 recs = []
-for div in tocpage.body.find_all('div', attrs = {'class' : 'article_result_list_contain_100'}):
+for div in tocpage.body.find_all('div', attrs = {'class' : 'clear'}):
     rec = {'jnl' : journals[journal]['jnl'], 'vol' : vol, 'issue' : iss, 
            'tc' : 'P', 'auts' : [], 'keyw' : [], 'refs' : []}
     if len(sys.argv) > 4:
         rec['tc'] = 'C'
         rec['cnum'] = sys.argv[4]
-    for li in div.find_all('li'):
-        for a in li.find_all('a'):
-            if a.has_attr('href') and re.search('javascript', a['href']):
-                articlelink = re.sub("^.*?'(.*?)','(.*?)'.*", r'http://koreascience.or.kr/article/ArticleFullRecord.jsp?cn=\1&ordernum=\2', a['href'])
-    print articlelink
-    os.system('lynx -source "%s" > /tmp/%s.%i' % (articlelink, jnlfilename, len(recs)))
-    artf = open('/tmp/%s.%i' % (jnlfilename, len(recs)), 'r')
-    #page = BeautifulSoup(urllib2.urlopen(articlelink, timeout=300))
+    for a in div.find_all('a'):
+        if a.has_attr('href') and re.search('society', a['href']):
+            rec['artlink'] = re.sub('.*=', 'http://koreascience.or.kr/article/', a['href']) + '.page'
+            if 'licence'  in journals[journal].keys():
+                rec['FFT'] = a['href']
+            recs.append(rec)
+
+
+i = 0
+for rec in recs:
+    i += 1
+    print '---{ %i/%i }---{ %s }---' % (i, len(recs), rec['artlink'])
+    if not os.path.isfile('/tmp/%s.%i' % (jnlfilename, i)):
+        time.sleep(3)
+        os.system('lynx -source "%s" > /tmp/%s.%i' % (rec['artlink'], jnlfilename, i))
+    artf = open('/tmp/%s.%i' % (jnlfilename, i), 'r')
+    #page = BeautifulSoup(urllib2.urlopen(rec['artlink'], timeout=300))
     page = BeautifulSoup(''.join(artf.readlines()))
     artf.close()
     #pbn
@@ -82,33 +98,41 @@ for div in tocpage.body.find_all('div', attrs = {'class' : 'article_result_list_
     for meta in page.head.find_all('meta', attrs = {'name' : 'citation_pdf_url'}):
         rec['FFT'] = meta['content']
     #license
-    if journal in ['jass']:
-        rec['licence'] = {'statement' : 'CC-BY-3.0'}
-    elif journal in ['jkms']:
-        rec['licence'] = {'statement' : 'CC-BY-NC'}
+    if 'licence'  in journals[journal].keys():
+        rec['licence'] = journals[journal]['licence']
     #kewyords
     for meta in page.head.find_all('meta', attrs = {'name' : 'citation_keywords'}):
         for keyw in re.split(';', meta['content']):
             rec['keyw'].append(re.sub('<TEX>', '', keyw))
     #authors
     for meta in page.head.find_all('meta', attrs = {'name' : 'citation_author'}):
-        if re.search(',', meta['content']):
-            rec['auts'].append(meta['content'])
-        else:
-            rec['auts'].append(re.sub('(.*) (.*)', r'\1, \2', meta['content']))
-    #abstract
-    for div in page.body.find_all('div', attrs = {'id' : 'abs'}):
-        rec['abs'] = div.text
-    #references
-    for div in page.body.find_all('div', attrs = {'class' : 'full_record_block'}):
-        for div2 in div.find_all('div', attrs = {'class' : 'full_record_text_r'}):
-            for a in div2.find_all('a'):
-                if a.has_attr('href') and re.search('dx.doi.org', a['href']):
-                    a.replace_with(re.sub('.*dx.doi.org.', ', DOI: ', a['href']))
-            rec['refs'].append([('x', re.sub('[\n\t]', '', div2.text))])
-    recs.append(rec)
+        for author in re.split(' *; *',  meta['content']):
+            if re.search(',', author):
+                rec['auts'].append(author)
+            else:
+                rec['auts'].append(re.sub('(.*) (.*)', r'\1, \2', author))
+    for div in page.body.find_all('div', attrs = {'class' : 'article-box'}):
+        for h4 in div.find_all('h4'):
+            #abstract
+            if re.search('Abstract', h4.text):
+                for p in div.find_all('p'):
+                    rec['abs'] = re.sub('[\r\n\t]', '', p.text)
+            #references
+            elif re.search('References', h4.text):
+                for li in div.find_all('li'):
+                    adoi = False
+                    for a in li.find_all('a'):
+                        if a.has_attr('href') and re.search('doi.org', a['href']):
+                            adoi = re.sub('.*doi.org.', ', DOI: ', a['href'])
+                            a.replace_with('')
+                    lit = li.text                    
+                    if not re.search('doi.org', lit) and adoi:
+                        lit += adoi
+#                    lit = re.sub('\.? *https?:\/\/doi.org\/', ', DOI: ', lit)
+                    rec['refs'].append([('x', re.sub('  +', '', re.sub('[\r\n\t]', ' ', lit)))])
+    print '   { ' + ' | '.join(['%s[%i] ' % (k[0], len(rec[k])) for k in rec.keys()]) + ' }---'
 
-
+    
 #write xml
 xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
 xmlfile  = codecs.EncodedFile(codecs.open(xmlf,mode='wb'),'utf8')
