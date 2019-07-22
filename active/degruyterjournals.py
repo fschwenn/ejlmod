328f6bd97978803c213ca073633d654bc3bb4bec
Florian Schwennsen <florian.schwennsen@desy.de>
2019-07-22 13:50:41 +0200

    degruyterjournals: change in nameing of article links

--- active/degruyterjournals.py
+++ active/degruyterjournals.py
@@ -37,7 +37,7 @@ if journal == 'phys':
 #get list of volumes
 if not os.path.isfile('/tmp/dg%s' % (jnlfilename)):
     print "%s/view/j/%s.%s.%s.issue-%s/issue-files/%s.%s.%s.issue-%s.xml" % (urltrunc, journal, year, vol, iss, journal, year, vol, iss)
-    os.system('wget -T 300 -t 3 -O -U "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0" /tmp/dg%s %s/view/j/%s.%s.%s.issue-%s/issue-files/%s.%s.%s.issue-%s.xml' % (jnlfilename, urltrunc, journal, year, vol, iss, journal, year, vol, iss))
+    os.system('wget -T 300 -t 3 -O /tmp/dg%s -U "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0"  %s/view/j/%s.%s.%s.issue-%s/issue-files/%s.%s.%s.issue-%s.xml' % (jnlfilename, urltrunc, journal, year, vol, iss, journal, year, vol, iss))
 inf = open('/tmp/dg%s' % (jnlfilename), 'r')
 tocpage = BeautifulSoup(''.join(inf.readlines()))
 inf.close()
@@ -48,7 +48,7 @@ for h3 in tocpage.find_all('h3'):
     for a in h3.find_all('a'):
         if a.has_attr('href') and re.search('view\/', a['href']):
             i += 1
-            vollink = a['href']
+            vollink = 'https://www.degruyter.com' + a['href']
             print vollink
             rec = {'tc' : 'P', 'jnl' : jnl, 'year' : year, 'vol' : vol, 'issue' : iss,
                    'auts' : [], 'aff' : [], 'keyw' : [], 'pacs' : []}
