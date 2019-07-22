534f7bf6d66b9783675da10a510a8e0155088d58
Florian Schwennsen <florian.schwennsen@desy.de>
2019-07-22 13:49:10 +0200

    ieee_crawler.xml: use selenium to download web pages

--- active/ieee_crawler.xml.py
+++ active/ieee_crawler.xml.py
@@ -12,6 +12,10 @@ import ejlmod2
 import codecs
 import time
 import json
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 absdir = '/afs/desy.de/group/library/abs'
 ejdir = '/afs/desy.de/user/l/library/dok/ejl'
@@ -63,11 +67,43 @@ def referencetostring(reference):
             refstring += ', doi: %s' % (re.sub('.*dx.doi.org\/','',a['href']))
     return refstring
 
+def translatejnlname(ieeename):
+    if ieeename in ["Applied Superconductivity, IEEE Transactions on", "IEEE Transactions on Applied Superconductivity"]:
+        jnlname = 'IEEE Trans.Appl.Supercond.'
+    elif ieeename in ["Nuclear Science, IEEE Transactions on",  "IEEE Transactions on Nuclear Science"]:
+        jnlname = 'IEEE Trans.Nucl.Sci.'
+    elif ieeename in ["IEEE Xplore: Magnetics, IEEE Transactions on", "Magnetics, IEEE Transactions on", 'IEEE Transactions on Magnetics']:
+        jnlname = 'IEEE Trans.Magnetics'
+    elif ieeename in ["IEEE Xplore: Microwave Theory and Techniques, IEEE Transactions on", "IEEE Xplore: IEEE Transactions on Microwave Theory and Techniques"]:
+        jnlname = 'IEEE Trans.Microwave Theor.Tech.'
+    elif ieeename in ["IEEE Xplore: Plasma Science, IEEE Transactions on", "IEEE Transactions on Plasma Science"]:
+        jnlname = 'IEEE Trans.Plasma Sci.'
+    elif ieeename in ["IEEE Xplore: Quantum Electronics, IEEE Journal of", "IEEE Xplore: IEEE Journal of Quantum Electronics"]:
+        jnlname = 'IEEE J.Quant.Electron.'
+    elif ieeename in ["Instrumentation and Measurement, IEEE Transactions on", "IEEE Xplore: IEEE Transactions on Instrumentation and Measurement", "IEEE Transactions on Instrumentation and Measurement"]:
+        jnlname = 'IEEE Trans.Instrum.Measur.'
+    elif re.search('^IEEE Xplore . Nuclear Science Symposium Conference Record', ieeename):
+        jnlname = 'IEEE Nucl.Sci.Symp.Conf.Rec.'
+    elif ieeename in ["Journal of Lightwave Technology"]:
+        jnlname = 'J.Lightwave Tech.'
+    elif ieeename in ["Instrumentation & Measurement Magazine, IEEE", "IEEE Instrumentation & Measurement Magazine"]:
+        jnlname = 'IEEE Instrum.Measur.Mag.'
+        tc = 'I'
+    elif ieeename in ["IEEE Symposium Conference Record Nuclear Science 2004.",
+                      "IEEE Nuclear Science Symposium Conference Record, 2005"]:
+        jnlname = 'BOOK'
+        tc = 'C'
+    else:
+        print 'unknown journal', ieeename
+        sys.exit(0)
+    return jnlname
+
 def ieee(number):
     urltrunc = "http://ieeexplore.ieee.org"
     #get name of journal
-    if number[0] == 'C':
-        toclink = "/xpl/mostRecentIssue.jsp?punumber=%s&rowsPerPage=2000" % (number[1:])
+    if number[0] == 'C': 
+        #toclink = "/xpl/mostRecentIssue.jsp?punumber=%s&rowsPerPage=2000" % (number[1:])        
+        toclink = "/xpl/conhome/%s/proceeding?rowsPerPage=2000" % (number[1:])
         tc = 'C'
         jnlname = 'IEEE Nucl.Sci.Symp.Conf.Rec.'
         jnlname = 'BOOK'
@@ -75,50 +111,33 @@ def ieee(number):
         toclink = "/xpl/tocresult.jsp?isnumber=%s&rowsPerPage=2000" % (number)
         tc = 'P'
     print 'getting TOC from %s%s' % (urltrunc, toclink)
-    page = BeautifulSoup(urllib2.urlopen(urltrunc + toclink,timeout=300))
-    for meta in page.head.find_all('meta'):
-        fsunwrap(meta)
-        if meta.attrs.has_key('name') and 'Title' in meta['name']:
-            if meta['content'] in ["IEEE Xplore - Applied Superconductivity, IEEE Transactions on", "IEEE Xplore - IEEE Transactions on Applied Superconductivity"]:
-                jnlname = 'IEEE Trans.Appl.Supercond.'
-            elif meta['content'] in ["IEEE Xplore - Nuclear Science, IEEE Transactions on",  "IEEE Xplore - IEEE Transactions on Nuclear Science"]:
-                jnlname = 'IEEE Trans.Nucl.Sci.'
-            elif meta['content'] in ["IEEE Xplore: Magnetics, IEEE Transactions on", "IEEE Xplore - Magnetics, IEEE Transactions on", 'IEEE Xplore - IEEE Transactions on Magnetics']:
-                jnlname = 'IEEE Trans.Magnetics'
-            elif meta['content'] in ["IEEE Xplore: Microwave Theory and Techniques, IEEE Transactions on", "IEEE Xplore: IEEE Transactions on Microwave Theory and Techniques"]:
-                jnlname = 'IEEE Trans.Microwave Theor.Tech.'
-            elif meta['content'] in ["IEEE Xplore: Plasma Science, IEEE Transactions on", "IEEE Xplore: "]:
-                jnlname = 'IEEE Trans.Plasma Sci.'
-            elif meta['content'] in ["IEEE Xplore: Quantum Electronics, IEEE Journal of", "IEEE Xplore: IEEE Journal of Quantum Electronics"]:
-                jnlname = 'IEEE J.Quant.Electron.'
-            elif meta['content'] in ["IEEE Xplore - Instrumentation and Measurement, IEEE Transactions on", "IEEE Xplore: IEEE Transactions on Instrumentation and Measurement", "IEEE Xplore - IEEE Transactions on Instrumentation and Measurement"]:
-                jnlname = 'IEEE Trans.Instrum.Measur.'
-            elif re.search('^IEEE Xplore . Nuclear Science Symposium Conference Record', meta['content']):
-                jnlname = 'IEEE Nucl.Sci.Symp.Conf.Rec.'
-            elif meta['content'] in ["IEEE Xplore - Journal of Lightwave Technology"]:
-                jnlname = 'J.Lightwave Tech.'
-            elif meta['content'] in ["IEEE Xplore - Instrumentation & Measurement Magazine, IEEE", "IEEE Xplore - IEEE Instrumentation & Measurement Magazine"]:
-                jnlname = 'IEEE Instrum.Measur.Mag.'
-                tc = 'I'
-            elif meta['content'] in ["IEEE Xplore - IEEE Symposium Conference Record Nuclear Science 2004.",
-                                     "IEEE Xplore - IEEE Nuclear Science Symposium Conference Record, 2005"]:
-                jnlname = 'BOOK'
-                tc = 'C'
-            else:
-                print 'unknown journal',meta['content']
-                sys.exit(0)
+
+# https://ieeexplore.ieee.org/xpl/tocresult.jsp?isnumber=8246765&rowsPerPage=2000
+    
+    articlelinks = []
+    driver = webdriver.PhantomJS()
+    driver.implicitly_wait(30)
+    #driver.delete_all_cookies()
+    driver.get(urltrunc + toclink)
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'icon-pdf')))
+    #clicl to accept cookies
+    driver.find_element_by_css_selector('.cc-btn.cc-dismiss').click()
+    time.sleep(3)
+    page = BeautifulSoup(driver.page_source)
     #get links to individual articles
     articlelinks = []
-    for a in page.body.find_all('a', attrs = {'class' : 'art-abs-url'}):
-        if a.attrs.has_key('href'):
+    for a in page.body.find_all('a'):
+        #print a
+        if a.text == 'View more':
             link = a['href']
             articlelinks.append(urltrunc+link)
+
+    print 'found %i article links' % (len(articlelinks))
     recs = []
     i = 0
     for articlelink in articlelinks:
         i += 1
-        print 'articlelink:', articlelink
-        rec = {'keyw' : [], 'jnl' : jnlname, 'autaff' : [], 'tc' : tc}
+        print '---{ %i/%i }---{ %s }---' % (i, len(articlelinks), articlelink)
         #rec['note'] = ['Konferenz ?']
         artfilename = '/tmp/ieee_%s.%i' % (number, i)
         if not os.path.isfile(artfilename):
@@ -130,24 +149,24 @@ def ieee(number):
                 time.sleep(60)
         inf = open(artfilename, 'r')
         articlepage = BeautifulSoup(''.join(inf.readlines()))
-
-#        try:
-#            articlepage = BeautifulSoup(urllib2.urlopen(articlelink,timeout=300))
-#        except:
-#            try:
-#                print "retry in 60 seconds"
-#                time.sleep(60)
-#                articlepage = BeautifulSoup(urllib2.urlopen(articlelink,timeout=300))
-#            except:
-#                print "retry in 600 seconds"
-#                time.sleep(600)
-#                articlepage = BeautifulSoup(urllib2.urlopen(articlelink,timeout=300))                
+        rec = {'keyw' : [], 'autaff' : [], 'refs' : []}
         #metadata now in javascript
         for script in articlepage.find_all('script', attrs = {'type' : 'text/javascript'}):
             if re.search('global.document.metadata', script.text):
                 gdm = re.sub('[\n\t]', '', script.text).strip()
                 gdm = re.sub('.*global.document.metadata=(\{.*\}).*', r'\1', gdm)
                 gdm = json.loads(gdm)
+        if gdm.has_key('publicationTitle'):
+            if number[0] == 'C':
+                jnlname = 'BOOK'
+                tc = 'C'
+            else:
+                jnlname = translatejnlname(gdm['publicationTitle'])
+                if jnlname == 'IEEE Instrum.Measur.Mag.':
+                    tc = 'I'
+                elif jnlname == 'BOOK':
+                    tc = 'C'
+            rec['jnl'] = jnlname
         if gdm.has_key('authors'):
             for author in gdm['authors']:
                 autaff = [author['name']]
@@ -165,7 +184,6 @@ def ieee(number):
                 rec['p1'] = gdm['startPage']
                 rec['p2'] = gdm['endPage']
         else:
-#            if gdm.has_key('endPageXXX'):
             if gdm.has_key('endPage'):
                 rec['p1'] = gdm['startPage']
                 rec['p2'] = gdm['endPage']
@@ -189,6 +207,8 @@ def ieee(number):
                 pass
         if gdm.has_key('doi'):
             rec['doi'] = gdm['doi']
+        else:
+            rec['doi'] = '30.3000/ieee_%s_%06i' % (number, i)  
         if gdm.has_key('keywords'):
             for kws in gdm['keywords']:
                 for kw in kws['kwd']:
@@ -203,14 +223,44 @@ def ieee(number):
             rec['issue'] = gdm['issue']
         if gdm.has_key('volume'):
             rec['vol'] = gdm['volume']
+        rec['tc'] = tc
         if gdm['isConference']:
             rec['tc'] = 'C'
-            rec['note'] = [gdm['publicationTitle']]
+            rec['note'] = [gdm['publicationTitle']]            
         if len(args) > 1:
             rec['cnum'] = args[1]  
             rec['tc'] = 'C'
         #references
-        #now hidden in JavaScript
+        try:
+            driver.get(articlelink + 'references')
+            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'stats-reference-link-googleScholar')))
+            refpage = BeautifulSoup(driver.page_source)
+        except:
+            print 'wait 3 minutes'
+            time.sleep(180)
+            try:
+                driver.get(articlelink + 'references')
+                WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'stats-reference-link-googleScholar')))
+                refpage = BeautifulSoup(driver.page_source)
+            except:
+                print '  could not load "%s%s"' % (articlelink, 'references')
+                refpage = page
+        for div in refpage.find_all('div', attrs = {'class' : 'reference-container'}):
+            for span in div.find_all('span', attrs = {'class' : 'number'}):
+                for b in span.find_all('b'):
+                    refnumber = re.sub('\.', '', span.text.strip())
+                    span.replace_with('[%s] ' % (refnumber))
+            for a in div.find_all('a', attrs = {'class' : 'stats-reference-link-crossRef'}):
+                rdoi = re.sub('.*doi.org\/(10.*)', r'\1', a['href'])
+                a.replace_with(', DOI: %s' % (rdoi))
+            for a in div.find_all('a', attrs = {'class' : 'stats-reference-link-googleScholar'}):
+                a.replace_with('')
+            ref = re.sub('[\n\t]', ' ', div.text.strip())
+            ref = re.sub('  +', ' ', ref)
+            rec['refs'].append([('x', ref)])
+        print '  found %i references' % (len(rec['refs']))
+        time.sleep(11)
+
                     
         if jnlname == 'IEEE Nucl.Sci.Symp.Conf.Rec.':
             try:
