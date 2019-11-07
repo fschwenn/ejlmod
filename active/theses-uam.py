85ed61c1cef7f991bbf25e303d01755fb71457cd
Florian Schwennsen <florian.schwennsen@desy.de>
2019-11-07 11:19:50 +0100

    theses-uam: new theses crawler for Madrid

---
+++ active/theses-uam.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+#harvest theses from University of U. Autonoma, Madrid (main) 
+#FS: 2019-09-26
+
+
+import getopt
+import sys
+import os
+import urllib2
+import urlparse
+from bs4 import BeautifulSoup
+import re
+import ejlmod2
+import codecs
+import datetime
+import time
+import json
+
+xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
+retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"
+
+
+now = datetime.datetime.now()
+stampoftoday = '%4d-%02d-%02d' % (now.year, now.month, now.day)
+
+publisher = 'U. Autonoma, Madrid (main)'
+
+typecode = 'T'
+
+jnlfilename = 'THESES-UAM-%s' % (stampoftoday)
+
+hdr = {'User-Agent' : 'Magic Browser'}
+tocurl = 'https://repositorio.uam.es/handle/10486/129587/discover?sort_by=dc.date.issued_dt&order=desc&rpp=10'
+print tocurl
+req = urllib2.Request(tocurl, headers=hdr)
+tocpage = BeautifulSoup(urllib2.urlopen(req))
+recs = []
+for div in tocpage.body.find_all('div', attrs = {'class' : 'artifact-description'}):
+    rec = {'tc' : 'T', 'keyw' : [], 'jnl' : 'BOOK', 'autaff' : [], 'supervisor' : []}
+    for a in div.find_all('a'):
+        rec['artlink'] = 'https://repositorio.uam.es' + a['href'] #+ '?show=full'
+        rec['hdl'] = re.sub('.*handle\/', '', a['href'])
+        recs.append(rec)
+
+i = 0
+for rec in recs:
+    i += 1
+    print '---{ %i/%i}---{ %s }------' % (i, len(recs), rec['artlink'])
+    try:
+        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
+        time.sleep(3)
+    except:
+        try:
+            print "retry %s in 180 seconds" % (rec['artlink'])
+            time.sleep(180)
+            artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
+        except:
+            print "no access to %s" % (rec['artlink'])
+            continue    
+    for meta in artpage.head.find_all('meta'):
+        if meta.has_attr('name'):
+            #author
+            if meta['name'] == 'DC.creator':
+                author = re.sub(' *\[.*', '', meta['content'])
+                rec['autaff'] = [[ author ]]            
+                rec['autaff'][-1].append('U. Autonoma, Madrid (main)')
+            #title
+            elif meta['name'] == 'DC.title':
+                rec['tit'] = meta['content']
+            #date
+            elif meta['name'] == 'DCTERMS.issued':
+                rec['date'] = meta['content']
+            #keywords
+            #elif meta['name'] == 'DC.subject':
+            #    for keyw in re.split(' *; *', meta['content']):
+            #        rec['keyw'].append(keyw)
+            #abstract
+            elif meta['name'] == 'DCTERMS.abstract':
+                if meta['xml:lang'] == 'en_US':
+                    rec['abs'] = meta['content']
+            #FFT
+            elif meta['name'] == 'citation_pdf_url':
+                rec['FFT'] = meta['content']
+            #pages
+            elif meta['name'] == 'DCTERMS.extent':
+                if re.search('\d pag', meta['content']):
+                    rec['pages'] = re.sub('\D', '', meta['content'])
+            #language
+            elif meta['name'] == 'DC.language':
+                if meta['content'] == 'spa':
+                    rec['language'] = 'spanish'
+                elif meta['content'] == 'fra':
+                    rec['language'] = 'French'
+                elif meta['content'] != 'eng':
+                    rec['note'] = [ 'Language: %s' % (meta['content']) ]            
+    #license
+    for a in artpage.body.find_all('a', attrs = {'rel' : 'license'}):
+        rec['license'] = {'url' : a['href']}
+
+
+
+
+#closing of files and printing
+xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
+xmlfile  = codecs.EncodedFile(codecs.open(xmlf,mode='wb'),'utf8')
+ejlmod2.writeXML(recs,xmlfile,publisher)
+xmlfile.close()
+#retrival
+retfiles_text = open(retfiles_path,"r").read()
+line = jnlfilename+'.xml'+ "\n"
+if not line in retfiles_text: 
+    retfiles = open(retfiles_path,"a")
+    retfiles.write(line)
+    retfiles.close()
