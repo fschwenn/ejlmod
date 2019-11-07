7694e7d705f64020b550589ef3ea271b07a68325
Florian Schwennsen <florian.schwennsen@desy.de>
2019-11-07 11:06:11 +0100

    theses-maryland: changes in toc page

--- active/theses-maryland.py
+++ active/theses-maryland.py
@@ -30,8 +30,7 @@ typecode = 'T'
 jnlfilename = 'THESES-MARYLAND-%s' % (stampoftoday)
 
 numberoftheses = 400
-tocurl = 'https://drum.lib.umd.edu/handle/1903/2800/browse?type=dateissued&submit_browse=Issue+Date&rpp=%i&order=DESC' % (numberoftheses)
-
+tocurl = 'https://drum.lib.umd.edu/handle/1903/2800/discover?filtertype=dateIssued&filter_relational_operator=equals&filter=[' + str(now.year-1) + '+TO+' + str(now.year+1) + ']&rpp=200'
 try:
     tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl))
     time.sleep(4)
@@ -41,47 +40,39 @@ except:
     tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl))
 
 recs = []
-for ul in tocpage.body.find_all('ul', attrs = {'class' : 'ds-artifact-list'}):
-    for li in ul.find_all('li'):
-        for h4 in li.find_all('h4'):
-            for a in h4.find_all('a'):
-                rec = {'jnl' : 'BOOK', 'tc' : 'T', 'tit' : a.text.strip(), 
-                       'keyw' : [], 'supervisor' : []}
-                rec['link'] = 'https://drum.lib.umd.edu' + a['href']
-                rec['doi'] = '20.2000/MARYLAND/' + re.sub('\W', '', a['href'])
-            for span in li.find_all('span', attrs = {'class' : 'date'}):
-                rec['date'] = span.text.strip()
-                year = int(re.sub('.*(20\d\d).*', r'\1', rec['date']))
-                if year >= now.year - 1*100:
-                    recs.append(rec)
+for div in tocpage.body.find_all('div', attrs = {'class' : 'artifact-description'}):
+    for a in div.find_all('a'):
+        rec = {'jnl' : 'BOOK', 'tc' : 'T', 'tit' : a.text.strip(), 
+               'keyw' : [], 'supervisor' : []}
+        rec['artlink'] = 'https://drum.lib.umd.edu' + a['href']
+        rec['hdl'] = re.sub('.*handle\/', '', a['href'])
+        if not  rec['hdl'] in ['1903/22153']:
+            recs.append(rec)
 
 i = 0
 for rec in recs:
     i += 1
-    print '---{ %i/%i }---{ %s }------' % (i, len(recs), rec['link'])
+    print '---{ %i/%i }---{ %s }------' % (i, len(recs), rec['artlink'])
     try:
-        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['link']))
+        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
         time.sleep(4)
     except:
         print "retry %s in 180 seconds" % (rec['link'])
         time.sleep(180)
-        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['link']))
+        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
     for meta in artpage.head.find_all('meta'):
         if meta.has_attr('name'):
             if meta['name'] == 'DCTERMS.abstract':
                 rec['abs'] = meta['content']
             elif meta['name'] == 'DC.subject':
                 rec['keyw'].append(meta['content'])
-            elif meta['name'] == 'DC.type':
-                if meta['content'] == "Dissertation":
-                    rec['MARC'] = [('502', [('d', rec['date']), ('c', 'Maryland U.'), ('b', 'PhD')])]                    
             elif meta['name'] == 'DC.creator':
                 rec['autaff'] = [[ meta['content'], 'Maryland U.' ]]
             elif meta['name'] == 'bepress_citation_pdf_url':
                 rec['FFT'] = meta['content']
             elif meta['name'] == 'DC.identifier':
                 if re.search('doi:', meta['content']):
-                    rec['doi'] = meta['content'][4:]
+                    rec['doi'] = re.sub('.*doi.org\/', '', meta['content'])
             elif meta['name'] == 'citation_pdf_url':
                 rec['FFT'] = meta['content']
             #elif meta['name'] == 'DC.contributor':
