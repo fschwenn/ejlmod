572ae2aff1d546e9f89232ef24b9bff6e8b58ff3
Florian Schwennsen <florian.schwennsen@desy.de>
2019-11-07 10:57:14 +0100

    theses-sissa: rewritten due to web page change

--- active/theses-sissa.py
+++ active/theses-sissa.py
@@ -29,69 +29,84 @@ typecode = 'T'
 
 jnlfilename = 'THESES-SISSA-%s' % (stampoftoday)
 
-tocurl = 'http://preprints.sissa.it/xmlui/handle/1963/3/recent-submissions?offset='
+tocurl = 'https://iris.sissa.it/simple-search?query=&location=&sort_by=score&order=desc&rpp=100&filter_field_1=publtypeh&filter_type_1=equals&filter_value_1=8+Thesis&filter_field_2=dateIssued&filter_type_2=equals&filter_value_2=%5B' + str(now.year - 1) + '+TO+' + str(now.year + 1) + '%5D&etal=0&filtername=publtypeh&filterquery=8+Thesis%3A%3A8.1+PhD+thesis&filtertype=equals'
 
+
+print tocurl
+
+hdr = {'User-Agent' : 'Magic Browser'}
 
 prerecs = []
 for offset in [0]:
-    try:
-        tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open('%s%i' % (tocurl, offset)))
-        time.sleep(3)
-    except:
-        print "retry %s%i in 180 seconds" % (tocurl, offset)
-        time.sleep(180)
-    tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open('%s%i' % (tocurl, offset)))
-    for ul in tocpage.body.find_all('ul', attrs = {'class' : 'ds-artifact-list'}):
-        for li in ul.find_all('li'):
-            rec = {'jnl' : 'BOOK', 'tc' : typecode, 'keyw' : [], 'supervisor' : []}
-            for div in li.find_all('div', attrs = {'class' : 'artifact-title'}):
-                for a in div.find_all('a'):
-                    rec['link'] = 'http://preprints.sissa.it' + a['href']
-                    rec['doi'] = '20.2000/SISSA/' + re.sub('\W', '', a['href'])
-                    rec['tit'] = a.text.strip()
-            for span in li.find_all('span', attrs = {'class' : 'publisher-date'}):
-                rec['date'] = re.sub('.*?(\d.*\d).*', r'\1', span.text.strip())
-            prerecs.append(rec)
+#    try:
+    req = urllib2.Request(tocurl, headers=hdr)
+    tocpage = BeautifulSoup(urllib2.urlopen(req))
+    time.sleep(3)
+#    except:
+#        print "retry in 180 seconds"
+#        time.sleep(180)
+#        tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open('%s%i' % (tocurl, offset)))
+    for tr in tocpage.body.find_all('tr'):
+        rec = {'tc' : 'T', 'keyw' : [], 'jnl' : 'BOOK'}
+        for td in tr.find_all('td', attrs = {'headers' : 't1'}):
+            for a in td.find_all('a'):
+                rec['artlink'] = 'https://iris.sissa.it' + a['href']
+                rec['hdl'] = re.sub('.*handle\/', '', a['href'])
+                prerecs.append(rec)
+
+        
 
 recs = []
 i = 0
 for rec in prerecs:
     i += 1
-    print '---{ %i/%i}---{ %s}------' % (i, len(prerecs), rec['link'])
-    year = int(re.sub('.*(20\d\d).*', r'\1', rec['date']))
-    if year < now.year - 1:
-        continue
+    print '---{ %i/%i}---{ %s}------' % (i, len(prerecs), rec['artlink'])
     try:
-        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['link']))
+        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
         time.sleep(3)
     except:
         try:
             print "retry %s in 180 seconds" % (rec['link'])
             time.sleep(180)
-            artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['link']))
+            artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
         except:
             print "no access to %s" % (rec['link'])
-            continue
+            continue      
     for meta in artpage.head.find_all('meta'):
         if meta.has_attr('name'):
-            if meta['name'] == 'DC.creator':
-                rec['auts'] = [ meta['content'] ]
-            elif meta['name'] == 'DCTERMS.abstract':
-                rec['abs'] = meta['content']
-            elif meta['name'] == 'DC.description':
-                rec['note'] = [ meta['content'] ]
+            #author
+            if meta['name'] == 'citation_author':
+                rec['autaff'] = [[ meta['content'] ]]
+            elif meta['name'] == 'citation_author_orcid':
+                rec['autaff'][-1].append( 'ORCID:' + meta['content'] )
+            elif meta['name'] == 'citation_author_email':
+                rec['autaff'][-1].append( 'EMAIL:' + meta['content'] )
+            #title
+            elif meta['name'] == 'citation_title':
+                rec['tit'] = meta['content']
+            #date
+            elif meta['name'] == 'DCTERMS.issued':
+                rec['date'] = meta['content']
+            #keywords
             elif meta['name'] == 'DC.subject':
-                rec['keyw'].append(meta['content'])
-            elif meta['name'] == 'DC.contributor':
-                if not meta['content'] in ['Physics', 'Neuroscience', 'Mathematics']:
-                    rec['supervisor'].append([ meta['content'] ])
-    for div in artpage.body.find_all('div', attrs = {'class' : 'file-link'}):
-        for a in div.find_all('a'):
-            rec['FFT'] = 'http://preprints.sissa.it' + a['href']
+                for keyw in re.split(' *; *', meta['content']):
+                    rec['keyw'].append(keyw)
+            #language
+            elif meta['name'] == 'DC.language':
+                if meta['content'] == 'ita':
+                    rec['language'] = 'italian'
+            #FFT
+            elif meta['name'] == 'citation_pdf_url':
+                rec['FFT'] = meta['content']
+    for p in artpage.body.find_all('p', attrs = {'class' : 'abstractEng'}):
+        rec['abs'] = p.text.strip()
     recs.append(rec)
 
 
-    
+
+	
+
+
 #closing of files and printing
 xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
 xmlfile  = codecs.EncodedFile(codecs.open(xmlf,mode='wb'),'utf8')
