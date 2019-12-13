f5eb72e38edd0ad27158a9e31d8df4432b034ac5
Florian Schwennsen <florian.schwennsen@desy.de>
2019-12-13 12:50:33 +0100

    armenianjp: completely rewritten

--- active/armenianjp.py
+++ active/armenianjp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 #program to harvest ARMENIAN JOURNAL OF PHYSICS
-# FS 2012-06-01
+# FS 2019-12-13
 
 import os
 import ejlmod2
@@ -9,6 +9,10 @@ import re
 import sys
 import unicodedata
 import string
+import urllib2
+import urlparse
+from bs4 import BeautifulSoup
+import time
 
 
 
@@ -20,91 +24,68 @@ def tfstrip(x): return x.strip()
 publisher = 'National Academy of Sciences of Armenia'
 jnl = 'armjp'
 vol = sys.argv[1]
-#issue = sys.argv[2]
+issue = sys.argv[2]
 year = str(int(vol)+2007)
 
-jnlfilename = jnl+vol
+jnlfilename = '%s%s.%s' % (jnl, vol, issue)
 jnlname = 'Armenian J.Phys.'
 issn = "1829-1171"
 
-#urltrunk = 'http://www.flib.sci.am/eng/journal/Phys'
-urltrunk = 'http://ajp.asj-oa.am'
+urltrunk = 'http://www.flib.sci.am/eng/journal/Phys'
+#urltrunk = 'http://ajp.asj-oa.am'
+tocurl = '%s/PV%sIss%s.html' % (urltrunk, vol, issue)
 
-
-recnr = 1
-print "get table of content of %s%s ..." %(jnlname,vol)
-#print "lynx -source \"%s/PV%sIss%s.html\" > %s/%s.toc" % (urltrunk,vol,issue,tmpdir,jnlfilename)
-#os.system("lynx -source \"%s/PV%sIss%s.html\" > %s/%s.toc" % (urltrunk,vol,issue,tmpdir,jnlfilename))
-tocfilname = "%s/%s.toc" % (tmpdir,jnlfilename)
-if not os.path.isfile(tocfilname):
-    os.system('lynx -source "%s/view/year/%s.html" > %s' % (urltrunk, year, tocfilname))
-tocfil = open(tocfilname, 'r')
-#for tline in  map(tfstrip,tocfil.readlines()):
-tocline = re.sub('\s\s+',' ',' '.join(map(tfstrip,tocfil.readlines())))
+hdr = {'User-Agent' : 'Magic Browser'}
+print "get table of content of %s %s,  No. %s via %s ..." % (jnlname, vol, issue, tocurl)
+req = urllib2.Request(tocurl, headers=hdr)
+tocpage = BeautifulSoup(urllib2.urlopen(req))
+time.sleep(1)
 recs = []
-for tline in re.split(' *<p> *',tocline):
-    if re.search('span class="person_name"',tline):
-        #print tline
-        if re.search('Letter of Radik Martirosyan', tline):
-            continue
-        rec = {}
-        rec['auts'] = []
-        rec['aff'] = []
-        rec['year'] = year
-        rec['vol'] = vol
-        rec['typ'] = ''
-        rec['jnl'] = jnlname        
-        rec['note'] = []
-        rec['tc'] = "P"
-        rec['keyw'] = []
-        rec['tit'] = re.sub('.*?<a href.*?> *(.*?)\.? *<\/a>.*',r'\1',tline)
-        rec['tit'] = re.sub(' *<\/?em> *', '', rec['tit'])
-        authors  = re.sub('.*?<span class="person_name">(.*)<\/span>.*',r'\1',tline)
-        authors  = re.sub(' *and +','; ',authors)
-        authors = re.sub('<.*?>', '', authors)
-        #print rec,authors
-        #authors = re.sub('(.*?), (.*?),',r'\1, \2;'.authors)
-        for aut in re.split(' *; *', authors):
-            aut = re.sub('\. ([A-Z])\.', r'.\1.',aut)
-            aut = re.sub('\.\-([A-Z])\.', r'.\1.',aut)
-            #rec['auts'].append(re.sub('^(.*) (.*?)$', r'\2, \1', aut))            
-            rec['auts'].append(aut)            
-        if rec['auts'] == []: continue
-        if re.search('<\/a>.*pp\.? *\d+ *\- *\d+',tline):
-            rec['p1'] = re.sub('.*?<\/a>.*pp\.? *(\d+) *\- *(\d+).*',r'\1',tline)
-            rec['p2'] = re.sub('.*?<\/a>.*pp\.? *(\d+) *\- *(\d+).*',r'\2',tline)
-            rec['issue'] =  re.sub('.*?<\/a>.*\((\d+)\). pp\.*', r'\1', tline)
-        else:
-            rec['p1'] = rec['p2'] = re.sub('.*?<\/a>.*p\.? *(\d+).*',r'\1',tline)
-            rec['issue'] =  re.sub('.*?<\/a>.*\((\d+)\). p\.*', r'\1', tline)
-        rec['issue'] = re.sub('.*Armenian Journal of Physics, \d+ \((\d+)\).*', r'\1', tline)
-        link = re.sub('.*?<a href=\"(.*?)\".*',r'\1',tline)
-        #more detais from abstract page
-        print "%s%s (%s) %s" % (rec['jnl'],vol,year,rec['p1'])
-        artfilname = tmpdir+"/"+jnlfilename+"."+rec['p1']
-        if not os.path.isfile(artfilname):
-            os.system("lynx -source \"%s\" > %s" %(link,artfilname))
-        artfil = open(artfilname,'r')
-        for aline in  map(tfstrip,artfil.readlines()):
-            if re.search('<meta name="eprints.datestamp"', aline):
-                rec['date'] = re.sub('.*content="(.*?) .*', r'\1', aline)
-            if re.search('href.*\.pdf',aline):
-                rec['FFT'] = re.sub('.*href=\"(.*?\.pdf)\".*',r'\1',aline)
-                rec['link'] = link
-            if re.search('<h2>Abstract.*<\/p>',aline):
-                rec['abs'] = re.sub('.*<h2>Abstract.*?<p.*?> *(.*?) *<\/p>.*',r'\1',aline)
-            if re.search('<meta name="DC.subject"', aline):
-                rec['keyw'].append(re.sub('.*content="\d*\.(.*)".*', r'\1', aline))
-            #if re.search('Subjects.*<\/a>',aline):
-            #    subject = re.sub('.*Subjects.*?html\">(.*?)<\/a>.*',r'\1',aline)
-            #    rec['note'].append(re.sub('Physics > \d*\.','',subject))
-            if re.search('You seem to be attempting to access an item that has been removed from the repository',aline):
-                rec['note'].append('item that has been removed')
-            if re.search('creativecommons.org', aline):
-                rec['licence'] = {'url' : re.sub('.*(http.*?)".*', r'\1', aline)}
-        #write record
-        print "REC",rec
-        recs.append(rec)
+for tr in tocpage.body.find_all('tr'):
+    rec = {'auts' : [], 'year' : year, 'vol' : vol, 'keyw' : [],
+           'issue' : issue, 'jnl' : jnlname, 'note' : [], 'tc' : "P"}
+    for a in tr.find_all('a'):
+        if a.has_attr('href'):
+            rec['link'] = a['href']
+            rec['doi'] = '20.2000/' + re.sub('\W', '', a['href'][7:])
+            recs.append(rec)
+
+i = 0
+for rec in recs:
+    i += 1
+    print '---{ %i/%i }---{ %s }------' % (i, len(recs), rec['link'])
+    artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['link']))
+    time.sleep(3)
+    #license
+    for a in artpage.find_all('a'):
+        if a.has_attr('href') and re.search('creativecommons.org', a['href']):
+            rec['license'] = {'url' : re.sub('\/legalcode$', '', a['href'])}
+    for meta in artpage.head.find_all('meta'):
+        if meta.has_attr('name'):
+            #author
+            if meta['name'] == 'eprints.creators_name':
+                rec['auts'].append(meta['content'])
+            #title
+            elif meta['name'] == 'eprints.title':
+                rec['tit'] = meta['content']
+            #date
+            elif meta['name'] == 'DC.date':
+                rec['date'] = meta['content']
+            #abstract
+            elif meta['name'] == 'eprints.abstract':
+                rec['abs'] = meta['content']
+            #pages
+            elif meta['name'] == 'eprints.pagerange':
+                rec['p1'] = re.sub('\-.*', '', meta['content'])
+                rec['p2'] = re.sub('.*\-', '', meta['content'])
+            #subject
+            elif meta['name'] == 'DC.subject':
+                rec['note'].append(meta['content'])
+            #PDF
+            elif meta['name'] == 'eprints.document_url':
+                if 'license' in rec.keys():
+                    rec['FFT'] = meta['content']
+    print ' ', rec.keys()                 
 
 xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
 xmlfile  = open(xmlf,'w')
