113b92a85e016d9e3469b12e714e2ef7c8a875bc
Florian Schwennsen <florian.schwennsen@desy.de>
2020-01-24 13:36:14 +0100

    npcs: several changes on web page

--- active/npcs.py
+++ active/npcs.py
@@ -51,10 +51,11 @@ typecode = 'P'
 note = ''
 recnr = 1
 recs = []
-for line in  map(tfstrip,tocfil.readlines()):
-    #print line
+lines = ' '.join(map(tfstrip,tocfil.readlines()))
+for line in  re.split('<HR>', lines):
+   # print line
     if re.search('pp\. *\d',line):
-        pages = re.sub('.*pp\. *([\d\-]+).*',r'\1',line)
+        pages = re.sub('.*pp\. *([\d\- ]+).*',r'\1',line)
     if re.search('HREF.*Abstract',line):
         url = re.sub('.*HREF=.*?(vol.*?html).*Abstract.*',r'\1',line)
         artfilname = "%s/%s.%s" %(tmpdir,jnlfilename,recnr)
@@ -74,7 +75,7 @@ for line in  map(tfstrip,tocfil.readlines()):
         #rec['filename'] = rec['pdf']
         rec['note'] = []
         if re.search('\-',pages):
-            (rec['p1'],rec['p2'])  = re.split('\-',pages)
+            (rec['p1'],rec['p2'])  = re.split(' *\- *',pages)
         else:
             rec['p1'] = rec['p2'] = pages
         rec['refs'] = []
@@ -83,58 +84,46 @@ for line in  map(tfstrip,tocfil.readlines()):
         flagabs = False
         flagkeyw = False
         title = ''
-        for aline in  map(tfstrip,artfil.readlines()):
+        alines = ' '.join(map(tfstrip,artfil.readlines()))
+        for aline in  re.split('<HR>', alines)[1:]:
             #print aline
+            #DOI
+            if re.search('<P><[Bb]>DOI', aline):
+                rec['doi'] = akzenteabstreifen(re.sub('.*<P><[Bb]>DOI:? *<\/[Bb]>.*?(10\.33581.*?) *<.*', r'\1', aline.strip()))
+                aline = re.sub('<P><[Bb]>DOI:? *<\/[Bb]>.*?<', '<', aline)
             #keywords 
-            if flagkeyw:
-                keywordline += ' '+re.sub('<\/P>.*','',aline)
+            if re.search('<P><[Bb]>Key words', aline):
+                keywords = akzenteabstreifen(re.sub('.*<P><[Bb]>Key words:? *<\/[Bb]> *(.*?)<.*', r'\1', aline.strip()))
+                rec['keyw'] = re.split(' *, *', keywords)
+                aline = re.sub('<P><[Bb]>Key words:? *<\/[Bb]>.*?<', '<', aline)
             #title
-            if re.search('<B>',aline):
-                rec['tit'] = re.sub('.*<B> *','',removehtmlgesocks(akzenteabstreifen(aline)))
-            elif rec.has_key('tit') and not re.search('<\/B>',rec['tit']):
-                rec['tit'] +=  ' '+ re.sub('^ +','',removehtmlgesocks(akzenteabstreifen(aline)))
-            #abstract-flags
-            elif re.search('<P>',aline) and not rec.has_key('abs') and not flagabs and rec.has_key('tit'):
-                flagabs = True
-                rec['abs'] = re.sub('.*<P>','',removehtmlgesocks(akzenteabstreifen(aline)))
-            elif flagabs:
-                rec['abs'] += re.sub('<P>.*','',removehtmlgesocks(akzenteabstreifen(aline)))
-                if re.search('[a-z].*<P>',aline) or re.search('^ *<BR> *$',aline):
-                    flagabs = False
-            #authors and affiliations
-            elif re.search('<I>',aline) and not rec.has_key('abs') and rec.has_key('tit'):
-                authors = re.sub('.*<I> *','',removehtmlgesocks(akzenteabstreifen(aline)))
-                authors = re.sub('<\/I>.*','',authors)
-                authors = re.sub(',? and ',', ',authors)
-                for aut in re.split(' *, *',authors):
-                    aut = re.sub('\. ([A-Z])\.', r'.\1.',aut)
-                    aut = re.sub('\. ([A-Z])\.', r'.\1.',aut)
-                    aut = re.sub('\.\-([A-Z])\.', r'.\1.',aut)
-                    #print '  ',aut
-                    rec['auts'].append(re.sub('^(.*) (.*?)$', r'\2, \1', aut))
-            #keywords
-            elif rec.has_key('abs') and re.search('<I>Key words',aline):
-                keywordline = ''
-                flagkeyw = True
-            elif flagkeyw and re.search('<\/P>',aline):
-                flagkeyw = False
+            if re.search('<[BB]>',aline):
+                rec['tit'] = re.sub('.*<[BB]>(.*?)<\/[BB]>.*', r'\1',aline).strip()
             #PDF:
-            elif re.search('HREF.* PDF',aline):
+            if re.search('HREF.* PDF',aline):
                 if re.search('.*HREF=.*?(cgi.*pdf).>.*',aline):
                     link = re.sub('.*HREF=.*?(cgi.*pdf).>.*',r'\1',aline)                
                     rec['pdf'] = 'http://www.j-npcs.org/'+link
                 else:
                     link = re.sub('.*HREF=.*?(online.*pdf).>.*',r'\1',aline)                
                     rec['pdf'] = 'http://www.j-npcs.org/'+link
-                #rec['FFT'] = rec['pdf']
                 rec['filename'] = rec['pdf']
                 doi1 = re.sub('.*\/(.*)\.pdf',r'\1',link)
+                aline = re.sub('<P>Full text.*', '', aline)
+            #abstract-flags
+            if re.search('<P>', aline) and not rec['auts']:
+                rec['abs'] = re.sub('.*<[pP]>(.*?)</[pP]>.*', r'\1', aline.strip())
+            #authors and affiliations
+            if re.search('<I>',aline) and not rec['auts']:
+                authors = re.sub('.*<I> *(.*?) *<\/I>.*', r'\1', aline.strip())
+                authors = akzenteabstreifen(re.sub(',? and ',', ',authors))
+                for aut in re.split(' *, *',authors):
+                    rec['auts'].append(re.sub('^(.*) (.*?)$', r'\2, \1', aut))
         #write record        
-        rec['tit'] = removehtmlgesocks(akzenteabstreifen(re.sub(' *<\/B> *','',rec['tit'])))
+        rec['tit'] = removehtmlgesocks(akzenteabstreifen(re.sub(' *<\/[BB]> *','',rec['tit'])))
         rec['tit'] = re.sub('<br>','',rec['tit'])
         rec['tit'] = re.sub('\. *$','',rec['tit'])
         rec['tit'] = re.sub('<img.*?>','???',rec['tit'])
-        rec['keyw'] = re.split(' *, *',re.sub('^ ','', removehtmlgesocks(akzenteabstreifen(keywordline))))
         print rec
         if rec.has_key('abs'):
             if len(rec['abs'])>5:
@@ -143,7 +132,7 @@ for line in  map(tfstrip,tocfil.readlines()):
                 rec['abs'] = re.sub('<sub>(.*?)<\/sub>',r'_\1',rec['abs'])
                 rec['abs'] = re.sub('<em>(.*?)<\/em>',r'\1',rec['abs'])
                 rec['abs'] = re.sub('<\/?[a-z].*?>','',rec['abs'])
-                rec['abs'] = re.sub('<img.*?>','???',rec['abs'])
+                rec['abs'] = akzenteabstreifen(re.sub('<img.*?>','???', rec['abs']))
             elif rec.has_key('note'):
                 rec['note'].append('record information might be incomplete')
         recs.append(rec)
