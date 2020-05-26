6e9ac3c4afc2de031ff0ea8f7a36cceac93d183f
fschwenn <florian.schwennsen@desy.de>
2020-05-26 16:27:03 +0200

    theses-waynestate: new code to harvest theses from Wayne State U.

---
+++ active/theses-waynestate.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+#harvest Wayne State U. theses
+#FS: 2020-04-29
+
+
+import getopt
+import sys
+import os
+import urllib2
+import urlparse
+from bs4 import BeautifulSoup
+import re
+import ejlmod2
+import codecs
+import datetime
+import time
+import json
+
+xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
+retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"
+
+now = datetime.datetime.now()
+stampoftoday = '%4d-%02d-%02d' % (now.year, now.month, now.day)
+
+publisher = 'Wayne State U., Detroit'
+
+pages = 1 
+
+jnlfilename = 'THESES-WAYNESTATE-%s' % (stampoftoday)
+
+basetocurl = 'https://digitalcommons.wayne.edu/oa_dissertations/index.'
+tocextension = 'html'
+
+
+recs = []
+date = False
+for i in range(pages):
+    tocurl = basetocurl + tocextension
+    print '==={ %i/%i }==={ %s }===' % (i+1, pages, tocurl)
+    try:
+        tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl), features="lxml")
+        time.sleep(3)
+    except:
+        print "retry %s in 180 seconds" % (tocurl)
+        time.sleep(180)
+        tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl), features="lxml")
+    for div in tocpage.body.find_all('div', attrs = {'id' : 'series-home'}):
+        for child in div.children:
+            try:
+                name = child.name
+            except:
+                continue
+            if name == 'h4':
+                for span in child.find_all('span'):
+                    date = span.text.strip()
+            elif name == 'p':
+                #year = int(re.sub('.*(20\d\d).*', r'\1', rec['date']))
+                if int(date) >= now.year - 1-100:
+                    if child.has_attr('class') and 'article-listing' in child['class']:
+                        rec = {'jnl' : 'BOOK', 'tc' : 'T', 'date' : date}
+                        for a in child.find_all('a'):                    
+                            rec['tit'] = a.text.strip()
+                            rec['artlink'] = a['href']
+                            a.replace_with('')
+                        recs.append(rec)
+    print '  ', len(recs)
+    tocextension = '%i.html' % (i+2)
+
+i = 0
+for rec in recs:
+    i += 1
+    print '---{ %i/%i }---{ %s }---' % (i, len(recs), rec['artlink'])
+    try:
+        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']), features="lxml")
+        time.sleep(3)
+    except:
+        print "retry %s in 180 seconds" % (rec['artlink'])
+        time.sleep(180)
+        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']), features="lxml")
+    for meta in artpage.head.find_all('meta'):
+        if meta.has_attr('name'):
+            #abstract
+            if meta['name'] == 'description':
+                rec['abs'] = meta['content']
+            #keywords
+            elif meta['name'] == 'keywords':
+                rec['keyw'] = re.split(', ', meta['content'])
+            #thesis type
+            #elif meta['name'] == 'bepress_citation_dissertation_name':
+            #    rec['note'] = [ meta['content'] ]
+            #    if meta['content'] == "Ph.D.":
+            #        rec['MARC'] = [('502', [('d', date), ('c', 'Wayne State U., Detroit'), ('b', 'PhD')])]
+            #author
+            elif meta['name'] == 'bepress_citation_author':
+                rec['autaff'] = [[ meta['content'], publisher ]]
+            #fulltext
+            elif meta['name'] == 'bepress_citation_pdf_url':
+                rec['FFT'] = meta['content']
+            #DOI
+            elif meta['name'] == 'bepress_citation_doi':
+                rec['doi'] = re.sub('^ht.*?\/10', '10', meta['content'])
+            #date
+            elif meta['name'] == 'bepress_citation_online_date':
+                rec['date'] = meta['content']
+    for div in artpage.body.find_all('div', attrs = {'id' : 'advisor1'}):
+        for p in div.find_all('p'):
+            rec['supervisor'] = [[ re.sub('^Dr. ', '', p.text.strip()) ]]
+    for div in artpage.body.find_all('div', attrs = {'id' : 'advisor2'}):
+        for p in div.find_all('p'):
+            rec['supervisor'].append( [re.sub('^Dr. ', '', p.text.strip())] )
+    if not rec.has_key('doi'):
+        rec['doi'] = '20.2000/WayneState/' + re.sub('\W', '', re.sub('.*edu', '', rec['artlink']))
+        rec['link'] = rec['artlink']
+    print rec.keys()
+
+    
+    
+#closing of files and printing
+xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
+xmlfile  = codecs.EncodedFile(codecs.open(xmlf,mode='wb'),'utf8')
+ejlmod2.writeXML(recs,xmlfile,publisher)
+xmlfile.close()
+#retrival
+retfiles_text = open(retfiles_path,"r").read()
+line = jnlfilename+'.xml'+ "\n"
+if not line in retfiles_text: 
+    retfiles = open(retfiles_path,"a")
+    retfiles.write(line)
+    retfiles.close()
