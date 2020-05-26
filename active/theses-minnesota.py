0cfcbca83255d8b23cee768fcc6dce35718b3f00
fschwenn <florian.schwennsen@desy.de>
2020-05-26 16:29:51 +0200

    theses-minnesota: new code to harvest theses from Minnesota U.

---
+++ active/theses-minnesota.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+#harvest theses from Minnesota U.
+#FS: 2020-05-05
+
+
+import getopt
+import sys
+import os
+import urllib2
+import urlparse
+from bs4 import BeautifulSoup
+import re
+import ejlmod2
+import codecs
+import datetime
+import time
+import json
+
+xmldir = '/afs/desy.de/user/l/library/inspire/ejl' #+ '/special'
+retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles" #+ '_special'
+
+now = datetime.datetime.now()
+stampoftoday = '%4d-%02d-%02d' % (now.year, now.month, now.day)
+
+publisher = 'Minnesota U.'
+
+rpp = 100
+pages = 1
+
+hdr = {'User-Agent' : 'Magic Browser'}
+jnlfilename = 'THESES-MINNESOTA-%s' % (stampoftoday)
+
+prerecs = []
+for page in range(pages):
+    tocurl = 'https://conservancy.umn.edu/handle/11299/45273/discover?sort_by=dc.date.issued_dt&order=desc&rpp=' + str(rpp) + '&page=' + str(page+1)
+    print '==={ %i/%i }==={ %s }===' % (page+1, pages, tocurl)
+    req = urllib2.Request(tocurl, headers=hdr)
+    tocpage = BeautifulSoup(urllib2.urlopen(req))
+    time.sleep(5)
+    for div in tocpage.body.find_all('div', attrs = {'class' : 'artifact-description'}):
+        rec = {'tc' : 'T', 'keyw' : [], 'jnl' : 'BOOK'}
+        for a in div.find_all('a'):
+            for h4 in a.find_all('h4'):
+                rec['artlink'] = 'https://conservancy.umn.edu' + a['href']# + '?show=full'
+                rec['hdl'] = re.sub('.*handle\/', '', a['href'])
+                prerecs.append(rec)
+
+    recs = []
+
+i = 0
+for rec in prerecs:
+    i += 1
+    print '---{ %i/%i }---{ %s }---' % (i, len(prerecs), rec['artlink'])
+    try:
+        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
+        time.sleep(5)
+    except:
+        try:
+            print "retry %s in 180 seconds" % (rec['artlink'])
+            time.sleep(180)
+            artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
+        except:
+            print "no access to %s" % (rec['link'])
+            continue      
+    for meta in artpage.head.find_all('meta'):
+        if meta.has_attr('name'):
+            #author
+            if meta['name'] == 'DC.creator':
+                author = re.sub(' *\[.*', '', meta['content'])
+                rec['autaff'] = [[ author ]]
+                rec['autaff'][-1].append(publisher)
+            #title
+            elif meta['name'] == 'DC.title':
+                rec['tit'] = meta['content']
+            #date
+            elif meta['name'] == 'DCTERMS.issued':
+                rec['date'] = meta['content']
+            #keywords
+            elif meta['name'] == 'DC.subject':
+                for keyw in re.split(' *; *', meta['content']):
+                    rec['keyw'].append(keyw)
+            #FFT
+            elif meta['name'] == 'citation_pdf_url':
+                rec['hidden'] = meta['content']
+            #abstract
+            elif meta['name'] == 'DCTERMS.abstract':
+                rec['abs'] = meta['content']
+    recs.append(rec)
+
+
+#closing of files and printing
+xmlf    = os.path.join(xmldir, jnlfilename+'.xml')
+xmlfile  = codecs.EncodedFile(codecs.open(xmlf, mode='wb'), 'utf8')
+ejlmod2.writeXML(recs, xmlfile, publisher)
+xmlfile.close()
+#retrival
+retfiles_text = open(retfiles_path, "r").read()
+line = jnlfilename+'.xml'+ "\n"
+if not line in retfiles_text: 
+    retfiles = open(retfiles_path, "a")
+    retfiles.write(line)
+    retfiles.close()
+        
