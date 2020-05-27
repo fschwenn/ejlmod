5c00fa6e2f20f9fa9b9abd031ff88169c7e44df5
fschwenn <florian.schwennsen@desy.de>
2020-05-27 07:50:46 +0200

    degruyterbooks: changes in landing page

--- active/degruyterbooks.py
+++ active/degruyterbooks.py
@@ -13,8 +13,13 @@ import codecs
 import urllib2
 import urlparse
 import time
+import datetime
 from bs4 import BeautifulSoup
 import datetime
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
@@ -23,63 +28,86 @@ publisher = 'De Gruyter'
 
 serial = sys.argv[1]
 
-jnlfilename = 'dg' + serial
-if serial == '129734':
+now = datetime.datetime.now()
+stampoftoday = '%4d-%02d-%02d' % (now.year, now.month, now.day)
+
+jnlfilename = 'dg' + serial + stampoftoday
+if serial == 'GSTMP':
     jnl = "De Gruyter Stud.Math.Phys."
+    tocurl = 'https://www.degruyter.com/view/serial/GSTMP-B?contents=toc-59654'
 
 #get list of volumes
-os.system("wget -T 300 -t 3 -q -O /tmp/dg%s %s%s%s" % (serial, urltrunc, '/view/serial/', serial))
-inf = open('/tmp/dg%s' % (serial), 'r')
-tocpage = BeautifulSoup(''.join(inf.readlines()))
-inf.close()
+#os.system("wget -T 300 -t 3 -q -O /tmp/dg%s %s" % (serial, tocurl))
+#inf = open('/tmp/dg%s' % (serial), 'r')
+#tocpage = BeautifulSoup(''.join(inf.readlines()))
+#inf.close()
+driver = webdriver.PhantomJS()
+driver.implicitly_wait(30)
+driver.get(tocurl)
+tocpage = BeautifulSoup(driver.page_source)
+
 #get volumes
 recs = []
 i = 0
-for a in tocpage.find_all('a', attrs = {'class' : 'tocLink'}):
-    i += 1
-    vollink = urltrunc + a['href']
-    print vollink
-    rec = {'tc' : 'B', 'jnl' : jnl}
-    #authors
-    for span in a.find_all('span', attrs = {'class' : 'authors'}):
-        authors = re.sub(': *$', '', span.text)
-        rec['auts'] = re.split(' *\/ *', authors)
-    for span in a.find_all('span', attrs = {'class' : 'editors'}):
-        authors = re.sub(': *$', '', span.text)
-        rec['auts'] = []
-        for editor in re.split(' *\/ *', authors):
-            rec['auts'].append(editor + ' (Ed.)')
-    #title
-    for span in a.find_all('span', attrs = {'class' : 'title'}):
-        rec['tit'] = span.text.strip()
-    #year
-    for span in a.find_all('span', attrs = {'class' : 'year'}):
-        rec['year'] = re.sub('.*?(\d+).*', r'\1', span.text.strip())
-    #volume
-    for span in a.find_all('span', attrs = {'class' : 'volNo'}):
-        rec['vol'] = re.sub('.*?(\d+).*', r'\1', span.text.strip())
-    #get details
-    os.system("wget -T 300 -t 3 -q -O /tmp/dg%s.%i %s" % (serial, i, vollink))
-    inf = open('/tmp/dg%s.%i' % (serial, i), 'r')
-    volpage = BeautifulSoup(''.join(inf.readlines()))
-    inf.close()
-    #abstract
-    rec['abs'] = ''
-    for div in volpage.find_all('div', attrs = {'class' : 'region-padding'}):
-        for h3 in div.find_all('h3', attrs = {'id' : 'overviewTitle'}):
-            for p in div.find_all('p'):
-                rec['abs'] += p.text
-    #isbn
-    for dd in volpage.find_all('dd', attrs = {'id' : 'isbn'}):
-        rec['isbn'] = dd.text
-    #number of pages
-    for dt in volpage.find_all('dt', attrs = {'id' : 'pages'}):
-        rec['pages'] = re.sub('.*?(\d+).*', r'\1', dt.text.strip())
-    #keywords
-    for dd in volpage.find_all('dd', attrs = {'id' : 'keywords'}):
-        rec['keyw'] = re.split(', ', dd.text)
-    print rec
-    recs.append(rec)
+divs = tocpage.body.find_all('div', attrs = {'class' : 'cover-image'})
+for div in divs:
+    for a in div.find_all('a'):
+        i += 1
+        rec = {'tc' : 'B', 'jnl' : jnl, 'auts' : [], 'isbns' : []}
+        rec['link'] = urltrunc + a['href']
+        print i, rec['link']
+        #get details
+        time.sleep(10)
+        os.system("wget -T 300 -t 3 -q -O /tmp/dg%s.%i %s" % (serial, i, rec['link']))
+        inf = open('/tmp/dg%s.%i' % (serial, i), 'r')
+        volpage = BeautifulSoup(''.join(inf.readlines()))
+        inf.close()
+        for meta in volpage.head.find_all('meta'):
+            if meta.has_attr('name'):
+                #title
+                if meta['name'] == 'citation_title':
+                    rec['tit'] = meta['content']
+                #keywords
+                elif meta['name'] == 'citation_keywords':
+                    rec['keyw'] = re.split('; ', meta['content'])
+                #ISBN
+                #elif meta['name'] == 'citation_isbn':
+                #    rec['isbn'] = meta['content']
+                #date
+                elif meta['name'] == 'citation_publication_date':
+                    rec['date'] = meta['content']
+                #date
+                elif meta['name'] == 'dc.identifier':
+                    rec['doi'] = meta['content']
+                #authors
+                elif meta['name'] == 'citation_author':
+                    rec['auts'].append(meta['content'])
+            elif meta.has_attr('property'):
+                #abstract
+                if meta['property'] == 'og:description':
+                    rec['abs'] = meta['content']
+        #volume
+        for div in volpage.find_all('div', attrs = {'class' : 'font-content '}):
+            for a in div.find_all('a', attrs = {'class' : 'c-Button--primary"'}):
+                atext = a.text.strip()
+                if re.search('\d$', atext):
+                    rec['vol'] = re.sub('.*\D', '', atext)
+        #DOI
+        #pages
+        for dd in volpage.find_all('dd', attrs = {'class' : 'pagesarabic'}):
+            rec['pages'] = re.sub('[\n\t\r]', '', dd.text.strip())
+        #ISBNS
+        for dd in volpage.find_all('dd', attrs = {'class' : 'publisherisbn'}):
+            rec['isbns'].append([('a', re.sub('[\n\t\r\-]', '', dd.text.strip()))])
+        #authors
+        if not rec['auts']:
+            for div in volpage.find_all('div', attrs = {'class' : 'content-box'}):
+                for h2 in div.find_all('h2'):
+                    if re.search('Author', h2.text):
+                        for strong in div.find_all('strong'):
+                            rec['auts'].append(strong.text.strip())
+        print rec.keys()
+        recs.append(rec)
     
     
 
