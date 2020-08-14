f25b43543b7718bf7c7b9777e636486cf1379326
Florian Schwennsen <florian.schwennsen@desy.de>
2020-08-14 13:07:36 +0200

    sigma: rewritten: now with BeautifulSoup

--- active/sigma.py
+++ active/sigma.py
@@ -7,12 +7,14 @@ import ejlmod2
 import re
 import sys
 import codecs
-from removehtmlgesocks import removehtmlgesocks
+import urllib2
+import urlparse
+from bs4 import BeautifulSoup
+import datetime
+import time
 
-ejdir = '/afs/desy.de/user/l/library/dok/ejl'
-lproc = '/afs/desy.de/user/l/library/proc'
-tmpdir = '/tmp'
-xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
+xmldir = '/afs/desy.de/user/l/library/inspire/ejl' #+ '/special/'
+retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles" #+ '_special'
 
 def tfstrip(x): return x.strip()
 
@@ -24,131 +26,104 @@ jnlfilename = 'sigma'+year+'_'+str(firstarticle)
 
 
 
+tocurl = 'http://www.emis.de/journals/SIGMA/%s/' % (year)
 
+hdr = {'User-Agent' : 'MagicBrowser'}
 
+req = urllib2.Request(tocurl, headers=hdr)
+tocpage = BeautifulSoup(urllib2.urlopen(req))
 
-print "get table of content..."
-os.system("wget -q -O %s/%s.toc http://www.emis.de/journals/SIGMA/%s/" %(tmpdir,jnlfilename,year))
-tocfil = open(tmpdir+"/"+jnlfilename+".toc",'r')
-for line in  tocfil.readlines():
-    if re.search('Volume',line):
-        vol = re.sub('.*Volume (\d+).*',r'\1',line).strip()
-    if re.search('SIGMA.*HREF',line):
-        lastarticle = int(re.sub('.*HREF=\"(\d+).*',r'\1',line))
-        break
-tocfil.close()
-
-recs = []
-for i in range(firstarticle,lastarticle+1):
-    rec = {}
-    rec['typ'] = ''
-    rec['jnl'] = jnl
-    rec['vol'] = vol
-    rec['year'] = year
-    rec['tc'] = "P"
-    rec['p1'] = rec['p2'] = rec['artnum'] = "%03i" %(i)
-    auts = []
-    affs = []
-    print "get article %s%s (%s) %03i" %(jnl, vol, year, i)
-    artfilename = "%s/%s.%03i" %(tmpdir,jnlfilename,i)
-    if not os.path.isfile(artfilename):
-        os.system("wget -q -O %s http://www.emis.de/journals/SIGMA/%s/%03i" %(artfilename,year,i))
-    artfil = open(artfilename,'r')
-    flautaff = False
-    flabs = False
-    flkw = False
-    flref = False
-    for line in map(tfstrip,artfil.readlines()):
-        line = removehtmlgesocks(line)
-        #informations in one line
-        if re.search('http\:\/\/arxiv.org',line) and not flref:
-            rec['arxiv'] = re.sub('.*arXiv\:(\d+\.\d+).*',r'\1',line)
-            rec['pages'] = re.sub('.*, *(\d+) pages.*',r'\1',line)
-        if re.search('http\:\/\/dx.doi.org',line) and not flref:
-            rec['doi'] = re.sub('.*dx.doi.org\/(10.3842.SIGMA.*?)\".*',r'\1',line)
-            doi1 = re.sub('(\(|\)|\/)','_',rec['doi'])
-        elif re.search('^<H3>',line) and not  re.search('<A HREF',line):
-            rec['tit'] = re.sub('<H3>(.*)<\/H3>',r'\1',line)
-        elif re.search('<BIG>.*pdf',line):
-            line = re.sub('.*HREF=\"(.*pdf)\".*',r'\1',line)
-            rec['filename'] = "http://www.emis.de/journals/SIGMA/%s/%03i/%s" %(year,i,line)
-            rec['pdf'] = rec['filename'] 
-        elif re.search('<I>Contribution to.*Proceedings',line):
-            rec['comments'] = [ "Conference: "+re.sub('.*\.\.\/(.*?)\.html.*',r'\1',line) ]
-            rec['tc'] = "C"
-        #informations spread over several lines (start/end marking)
-        if re.search('<BLOCKQUOTE>',line): 
-            flautaff = True
-        elif re.search('(<\/BLOCKQUOTE>|<BIG>)',line): 
-            flautaff = False
-            rec['auts'] = auts
-            rec['aff'] = affs
-        if re.search('<B>Abstract',line):  
-            flabs = True
-            abs = ""
-        elif re.search('<\/P>',line):  flabs = False
-        if re.search('<B>Key words',line): 
-            flkw = True
-            kws = []
-        if re.search('<B>References',line): 
-            flref = True
+prerecs = {}
+for dl in tocpage.body.find_all('dl'):
+    rec = {'jnl' : jnl, 'year' : year, 'tc' : 'P', 'auts' : [], 'aff' : [], 'note' : []}
+    #title
+    for dt in dl.find_all('dt'):
+        for b in dt.find_all('b'):
+            rec['tit'] = b.text.strip()
+    for dd in dl.find_all('dd'):
+        ddt = re.sub('[\n\t\r]', ' ', dd.text.strip())
+        parts = re.split('SIGMA', ddt)
+        #p1
+        p1 = int(re.sub('.*\), (\d+), .*', r'\1', parts[1]))
+        rec['p1'] = '%03i' % (p1)
+        #pages
+        rec['pages'] = re.sub('.*?(\d+) page.*', r'\1', parts[1])
+    if p1 >= firstarticle:
+        time.sleep(1)
+        artlink = 'https://www.emis.de/journals/SIGMA/%s/%03i/' % (year, p1)
+        req = urllib2.Request(artlink, headers=hdr)
+        tocpage = BeautifulSoup(urllib2.urlopen(req))
+        for meta in tocpage.head.find_all('meta'):            
+            if meta.has_attr('name'):
+                #volume
+                if meta['name'] == 'citation_volume':
+                    rec['vol'] = meta['content']
+                #date
+                elif meta['name'] == 'citation_publication_date':
+                    rec['date'] = meta['content']
+                #DOI
+                elif meta['name'] == 'citation_doi':
+                    rec['doi'] = meta['content']
+                #fulltext
+                elif meta['name'] == 'citation_pdf_url':
+                    rec['pdf'] = meta['content']
+                #keywords
+                elif meta['name'] == 'citation_keywords':
+                    rec['keyw'] = re.split('; ', meta['content'])
+                #bull
+                elif meta['name'] == 'citation_arxiv_id':
+                    rec['arxiv'] = meta['content']
+        bqs = tocpage.body.find_all('blockquote')
+        #affiliations in superscripts
+        for sup in bqs[0].find_all('sup'):    
+            if re.search('\)', sup.text):
+                aff = re.sub('(.*)\)', r' XXX Aff\1= ', sup.text)
+                sup.replace_with(aff)
+            else:
+                aff = ', =Aff' + sup.text + ', '
+                sup.replace_with(aff)
+        #authors
+        for b in bqs[0].find_all('b'):
+            bt = re.sub('[\n\t\r]', ' ', b.text.strip())
+            for aut in re.split(' *, *', re.sub(' and ', ', ', bt)):
+                if len(aut) > 2:
+                    rec['auts'].append(aut)
+            b.decompose()
+        #affiliations
+        bqt = re.sub('[\n\t\r]', ' ', bqs[0].text.strip())
+        for aff in re.split('XXX ', bqt):
+            if len(aff) > 2:
+                rec['aff'].append(aff)                        
+        #abstract
+        for p in tocpage.body.find_all('p'):
+            for b in p.find_all('b'):
+                if b.text.strip() == 'Abstract':
+                    b.decompose()
+                    rec['abs'] = p.text.strip()
+        #conference?
+        for i in tocpage.body.find_all('i'):
+            if  re.search('Contribution to.*Proceedings', i.text):
+                rec['note'].append(i.text.strip())
+                rec['tc'] = 'C'
+        #refernces
+        for ol in tocpage.body.find_all('ol'):
             rec['refs'] = []
-            refno = 0
-            #reffile.write('add;\n DOI = '+rec['doi']+';\n')
-            #reffile.write(' SOURCE = '+publisher+';\n')
-        elif re.search('<\/OL>',line): 
-            flref = False
-            #reffile.write(';\n')
-        #informations spread over several lines (getting informations)
-        if flautaff:
-            if re.search('<B>',line):
-                line = re.sub('<\/?[PB]>', '', line)
-                line = re.sub(' and ', ', ', line)
-                line = re.sub('<SMALL><SUP>(\w+)<\/SUP><\/SMALL>', r', =Aff\1', line)
-                line = re.sub('<SUP><SMALL>(\w+)<\/SMALL><\/SUP>', r', =Aff\1', line)
-                for au in re.split(' *, *',line):
-                    auts.append(re.sub('^(.*) (.*?)$', r'\2, \1', au))
-            elif re.search('<I>',line):
-                line = re.sub('<BR><SMALL><SUP>(\w+)\)<\/SUP> *<I> *', r'Aff\1= ', line)
-                line = re.sub('<BR><SMALL><I> *', r'', line)
-                line = re.sub('<\/I><\/SMALL>','',line)
-                affs.append(line)
-        elif flabs:
-            abs += ' '+re.sub('(<P><B>Abstract<\/B>|<BR>)','',line)            
-            rec['abs'] = re.sub(' +', ' ',re.sub('<\/?[IB]>','',abs))
-        elif flkw:
-            if re.search('<\/P>',line): flkw = False
-            parts = re.split('; ', re.sub('(.*<\/B>|<\/?[PIB]>)','',line))
-            for part in parts:
-                if len(part)>1: kws.append(part)
-            rec['keyw'] = kws
-        elif flref:
-            #line = re.sub('<A HREF=\"http\:\/\/arxiv.org.*?\">','',line)
-            #line = re.sub('<\/?[ABI]>','',line)
-            if re.search('^<LI>',line):
-                refno += 1
-                raffle = re.sub('<LI>','',line)
-                iref = [('o', str(refno))]
-            elif refno > 0:
-                raffle += line
-            if refno > 0 and re.search('<\/LI>',raffle):
-                #print raffle
-                if re.search('arxiv.org',raffle):
-                    refbull = re.sub('.*arxiv.org.*?>(.*?)<.*',r'\1',raffle)
-                    iref.append(('r', refbull))
-                if re.search('dx.doi.org',raffle):
-                    refdoi = re.sub('.*dx.doi.org\/(10.*?)\".*',r'\1',raffle)
-                    raffle = re.sub('<A HREF.*?>','',raffle) # +' (doi: '+refdoi+')'
-                    iref.append(('a', refdoi))
-                raffle = re.sub(';',',',raffle)
-                iref.append(('m', re.sub('.*?<LI>(.*?)<\/LI>.*',r'\1',raffle)))
-                if len(iref) == 1:
-                    iref = [('x', re.sub('.*?<LI>(.*?)<\/LI>.*',r'\1',raffle))]
-                rec['refs'].append(iref)
-    #write record
-    print rec
-    recs.append(rec)
+            for li in ol.find_all('li'):
+                rdoi = False
+                for a in li.find_all('a'):
+                    if re.search('doi.org\/10', a['href']):
+                        rdoi = re.sub('.*org\/(10.*)', r', DOI: \1', a['href'])
+                ref = li.text.strip()
+                if rdoi:
+                    ref = re.sub('\.$', '', ref)
+                    ref += rdoi
+                rec['refs'].append([('x', ref)])
+        print p1, rec['p1'], rec['doi'], rec.keys()
+        prerecs[p1] = rec
 
+keys = prerecs.keys()
+keys.sort()
+recs = [prerecs[key] for key in keys]
                 
 
 xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
