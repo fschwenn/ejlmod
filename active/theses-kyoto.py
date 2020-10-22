4d5d4b6989c930d02268c84222c5d8ca3750e3a9
Florian Schwennsen <florian.schwennsen@desy.de>
2020-10-22 12:20:39 +0200

    theses-kyoto: handle bad certificate

--- active/theses-kyoto.py
+++ active/theses-kyoto.py
@@ -15,6 +15,7 @@ import codecs
 import datetime
 import time
 import json
+import ssl
 
 xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
 retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"
@@ -29,11 +30,15 @@ typecode = 'T'
 
 jnlfilename = 'THESES-KYOTO-%s' % (stampoftoday)
 
+#bad certificate
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 hdr = {'User-Agent' : 'Magic Browser'}
 tocurl = 'https://www-he.scphys.kyoto-u.ac.jp/theses/index.html'
 print tocurl
 req = urllib2.Request(tocurl, headers=hdr)
-tocpage = BeautifulSoup(urllib2.urlopen(req))
+tocpage = BeautifulSoup(urllib2.urlopen(req, context=ctx))
 recs = []
 table = tocpage.body.find_all('table')[0]
 for thead in table.find_all('thead'):
