7ccabb35e790b62bf35be61b870e508bf55bd2d1
Florian Schwennsen <florian.schwennsen@desy.de>
2020-11-09 17:34:01 +0100

    theses-hamburg: large rewrite due to changex in website

--- active/theses-hamburg.py
+++ active/theses-hamburg.py
@@ -28,23 +28,27 @@ publisher = 'U. Hamburg (main)'
 typecode = 'T'
 
 pages = 3
+rpp = 10
 
 hdr = {'User-Agent' : 'Magic Browser'}
 
-for fac in ['510', '530']:
+for fac in ['510+Mathematik', '530+Physik']:
     recs = []
     print '==={ %s }===' % (fac)
     for page in range(pages):
         time.sleep(1)
-        tocurl = 'https://ediss.sub.uni-hamburg.de/ergebnis.php?startindex=10&dir=&page=' + str(page+1) + '&suchfeld1=o.sachgruppe_ddc&suchwert1=' + fac + '&suchfeld2=person&suchwert2=&suchfeld3=date_year&suchwert3=&opt1=AND&opt2=AND&suchart=exakt&sort=o.date_year%20DESC,%20o.title&sprache=&Lines_Displayed=10&la=de'
+        #tocurl = 'https://ediss.sub.uni-hamburg.de/ergebnis.php?startindex=10&dir=&page=' + str(page+1) + '&suchfeld1=o.sachgruppe_ddc&suchwert1=' + fac + '&suchfeld2=person&suchwert2=&suchfeld3=date_year&suchwert3=&opt1=AND&opt2=AND&suchart=exakt&sort=o.date_year%20DESC,%20o.title&sprache=&Lines_Displayed=10&la=de'
+        tocurl = 'https://ediss.sub.uni-hamburg.de/simple-search?query=&location=&filter_field_1=subject&filter_type_1=equals&filter_value_1=' + fac + '&crisID=&relationName=&sort_by=bi_sort_2_sort&order=desc&rpp=' + str(rpp) + '&etal=0&start=' + str(page*rpp)
+
+
         print tocurl
         req = urllib2.Request(tocurl, headers=hdr)
         tocpage = BeautifulSoup(urllib2.urlopen(req))
         for tr in tocpage.body.find_all('tr'):
-            for td in tr.find_all('td', attrs = {'valign' : 'top'}):
-                rec = {'tc' : 'T', 'keyw' : [], 'jnl' : 'BOOK'}
-                for a in tr.find_all('a'):
-                    rec['artlink'] = a['href']
+            for td in tr.find_all('td', attrs = {'headers' : 't1'}):
+                rec = {'tc' : 'T', 'keyw' : [], 'jnl' : 'BOOK', 'supervisor' : []}
+                for a in td.find_all('a'):
+                    rec['artlink'] = 'https://ediss.sub.uni-hamburg.de' + a['href']
                     recs.append(rec)
     i = 0
     for rec in recs:
@@ -68,27 +72,44 @@ for fac in ['510', '530']:
                     author = re.sub(' *\[.*', '', meta['content'])
                     rec['autaff'] = [[ author ]]
                     rec['autaff'][-1].append(publisher)
+                #abstract
+                elif meta['name'] == 'DCTERMS.abstract':
+                    if meta.has_attr('xml:lang'):
+                        if meta['xml:lang'] == 'en':
+                            rec['abs'] = meta['content']
+                        elif meta['xml:lang'] == 'de':
+                            rec['absde'] = meta['content']
+                    else:
+                        rec['abs'] = meta['content']
+                #supervisor
+                elif meta['name'] == 'DC.contributor':
+                    rec['supervisor'].append([re.sub(' *\(.*', '', meta['content'])])
                 #title
                 elif meta['name'] == 'citation_title':
                     rec['tit'] = meta['content']
                 #date
-                elif meta['name'] == 'citation_date':
+                elif meta['name'] == 'DCTERMS.issued':
                     rec['date'] = meta['content']
                 #keywords
                 elif meta['name'] == 'DC.subject':
                     for keyw in re.split(' *, *', meta['content']):
                         rec['keyw'].append(keyw)
+                #language
+                elif meta['name'] == 'language':
+                    if meta['content'] == 'de':
+                        rec['language'] = 'german'
                 #urn
-                elif meta['name'] == 'DC.Identifier':
+                elif meta['name'] in ['DC.Identifier', 'DC.identifier']:
                     if re.search('^urn:', meta['content']):
                         rec['urn'] = meta['content']
                     else:
                         rec['link'] = meta['content']
-        for tr in artpage.body.find_all('tr'):
-            tds = tr.find_all('td', attrs = {'class' : 'frontdoor'})
-            if len(tds) == 2:
-                if re.search('Kurzfassung.*Englisch', tds[0].text):
-                    rec['abs'] = tds[1].text.strip()
+                #fulltext
+                elif meta['name'] == 'citation_pdf_url':
+                    rec['hidden'] = meta['content']
+        #abstract
+        if not 'abs' in rec.keys() and 'absde' in rec.keys():
+            rec['abs'] = rec['absde']
         print rec.keys()
 
     jnlfilename = 'THESES-HAMBURG-%s_%s' % (stampoftoday, fac)
