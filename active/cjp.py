7593bfd384eae71eb0d18a5fd630432b696c5e2d
Florian Schwennsen <florian.schwennsen@desy.de>
2020-11-16 14:48:51 +0100

    cjp: rewrite because web page changed

--- active/cjp.py
+++ active/cjp.py
@@ -13,6 +13,10 @@ import urllib2
 import urlparse
 from bs4 import BeautifulSoup
 import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
 tmpdir = '/tmp'
@@ -28,7 +32,7 @@ month = {'January' : 1, 'February' : 2, 'March' : 3, 'April' : 4,
          'May' : 5, 'June' : 6, 'July' : 7, 'August' : 8,
          'September' : 9, 'October' : 10, 'November' : 11, 'December' : 12}
 
-if   (jnl == 'cjp'): 
+if   (jnl == 'cjp'):
     jnlname = 'Can.J.Phys.'
     issn = '0008-4204'
 
@@ -46,39 +50,35 @@ except:
     time.sleep(180)
     tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl))
 
-
-grdivs = tocpage.body.find_all('div', attrs = {'class' : 'articleGroup'})
-if not grdivs:
-    grdivs = tocpage.find_all('body')
-
 recs = []
-for grdiv in grdivs:
-    section = False
-    for h2 in grdiv.find_all('h2'):
-        section = h2.text.strip()
-    for div in grdiv.find_all('div', attrs = {'class' : 'abstractAndAccess'}):
-        for a in div.find_all('a'):
-            if re.search('http', a['href']):
-                rec = {'jnl' : jnlname, 'vol' : vol, 'issue' : isu, 'tc' : 'P',
-                       'note' : [], 'aff' : [], 'auts' : []}
-                rec['artlink'] = a['href']
-                rec['doi'] = re.sub('.*org.(10.*)', r'\1', a['href'])
-                if section: rec['note'].append(section)
-                recs.append(rec)
-
+for div in tocpage.body.find_all('div', attrs = {'class' : 'table-of-content'}):
+    sectiontitle = False
+    for child in div.children:
+        try:
+            child.name
+        except:
+            continue
+        for h3 in child.find_all('h3'):
+            sectiontitle = h3.text.strip()
+        for div in child.find_all('div', attrs = {'class' : 'issue-item__title'}):
+            for a in div.find_all('a'):
+                for h5 in a.find_all('h5'):
+                    rec = {'jnl' : jnlname, 'vol' : vol, 'issue' : isu, 'tc' : 'P',
+                           'note' : [], 'autaff' : [], 'keyw' : []}
+                    rec['artlink'] = 'https://cdnsciencepub.com' + a['href']
+                    rec['doi'] = re.sub('.*?(10.1139.*)', r'\1', a['href'])
+                    if sectiontitle: rec['note'].append(sectiontitle)
+                    recs.append(rec)
 
 i = 0
+driver = webdriver.PhantomJS()
+driver.implicitly_wait(60)
 for rec in recs:
     i += 1
     print '---{ %i/%i }---{ %s }---' % (i, len(recs), rec['artlink'])
-    artfile = '/tmp/%s%s%s.%s' % (jnl, vol, isu, re.sub('/','_',rec['doi']))
-    if not os.path.isfile(artfile):
-        os.system('wget  -T 300 -t 3 -q -O %s "%s"' % (artfile, rec['artlink']))
-        time.sleep(2)
-    inf = open(artfile, 'r')
-    artpage = BeautifulSoup(''.join(inf.readlines()))
-    inf.close()    
-    for meta in artpage.head.find_all('meta'):
+    driver.get(rec['artlink'])
+    artpage = BeautifulSoup(driver.page_source, features="lxml")
+    for meta in artpage.find_all('meta'):
         if meta.has_attr('name'):
             #title
             if meta['name'] == 'dc.Title':
@@ -99,75 +99,74 @@ for rec in recs:
                 if meta['content'] == 'fr':
                     rec['language'] = 'French'
     #abstract
-    for div in artpage.body.find_all('div', attrs = {'class' : 'abstractSection'}):
-        rec['abs'] = div.text.strip()
+    for section in artpage.body.find_all('section', attrs = {'id' : 'abstract'}):
+        for h2 in section.find_all('h2'):
+            h2.decompose()
+        rec['abs'] = section.text.strip()
         if re.search('PACS Nos.: ', rec['abs']):
             pacss = re.sub('.*PACS Nos.: *', '', rec['abs'])
             rec['abs'] = re.sub('PACS Nos.*', '', rec['abs'])
-            rec['pacs'] = re.split(' *, *', pacss)  
+            rec['pacs'] = re.split(' *, *', pacss)
     #pages, year
-    for p in artpage.body.find_all('p', attrs = {'class' : 'citationLine'}):
-        for a in p.find_all(['a', 'span']):
-            a.replace_with('')
-        pbn = p.text.strip()
-        rec['year'] = re.sub('.*?([12]\d\d\d).*', r'\1', pbn)
-        p1p2 = re.sub('.*: *(.*?),.*', r'\1', pbn)
-        if p1p2 == pbn:
-            rec['p1'] = '0'
-            rec['p2'] = '0'
-        else:
-            if re.search('\-', p1p2):
-                [rec['p1'], rec['p2']] = re.split('\-', p1p2)
-            else:
-                rec['p1'] = p1p2
-                rec['p2'] = p1p2
-                rec['pages'] = '1'
-    #affiliations
-    for p in artpage.body.find_all('p', attrs = {'class' : 'affiliation'}):
-        for sup in p.find_all('sup'):
-            supt = sup.text
-            sup.replace_with('Aff%s= ' % (supt))
-        rec['aff'].append(p.text.strip())
-        p.replace_with('')
+    for span in artpage.body.find_all('span', attrs = {'property' : 'datePublished'}):
+        rec['year'] = span.text.strip()
+    for span in artpage.body.find_all('span', attrs = {'property' : 'pageStart'}):
+        rec['p1'] = span.text.strip()
+    for span in artpage.body.find_all('span', attrs = {'property' : 'pageEnd'}):
+        rec['p2'] = span.text.strip()
+    #keywords
+    for section in artpage.body.find_all('section', attrs = {'property' : 'keywords'}):
+        if section.has_attr('xml:lang') and section['xml:lang'] == 'fr':
+            continue
+        for li in section.find_all('li'):
+            keyword = li.text.strip()
+            if not keyword in rec['keyw']:
+                rec['keyw'].append(keyword)
     #authors
-    for p in artpage.body.find_all('p', attrs = {'class' : 'author'}):
-        for sup in p.find_all('sup'):
-            supt = sup.text
-            sup.replace_with(', =Aff%s, ' % (supt))
-        for author in re.split(',', re.sub(' and ', ',', re.sub('[\n\t\r]', ' ', p.text.strip()))):
-            author = author.strip()
-            if author:
-                rec['auts'].append(author)
+    for section in artpage.body.find_all('section', attrs = {'class' : 'core-authors'}):
+        for div in section.find_all('div', attrs = {'property' : 'author'}):
+            #name
+            for author in div.find_all('div', attrs = {'class' : 'heading'}):
+                for fn in author.find_all('span', attrs = {'property' : 'familyName'}):
+                    name = fn.text.strip()
+                for gn in author.find_all('span', attrs = {'property' : 'givenName'}):
+                    name += ', ' + gn.text.strip()
+                rec['autaff'].append([name])
+                #email
+                for email in author.find_all('a', attrs = {'property' : 'email'}):
+                    rec['autaff'][-1].append(re.sub('mailto:', 'EMAIL:', email['href']))
+            #affiliations
+            for aff in div.find_all('div', attrs = {'property' : 'organization'}):
+                rec['autaff'][-1].append(aff.text.strip())
     #references
-    for ul in artpage.body.find_all('ul', attrs = {'class' : 'no-bullet'}):
+    for section in artpage.body.find_all('section', attrs = {'id' : 'bibliography'}):
         rec['refs'] = []
-        for li in ul.find_all('li'):
+        for div in section.find_all('div', attrs = {'class' : 'labeled'}):
             rdoi = False
-            for a in li.find_all('a'):
+            for a in div.find_all('a'):
                 if re.search('rossref', a.text):
-                    rdoi = re.sub('.*key=', '', a['href'])
+                    rdoi = re.sub('.*doi.org\/', '', a['href'])
                     rdoi = re.sub('%2F', '/', rdoi)
                     rdoi = re.sub('%28', '(', rdoi)
                     rdoi = re.sub('%29', ')', rdoi)
                     rdoi = re.sub('%3A', ':', rdoi)
                 a.replace_with('')
-            reference = [('x', re.sub(', *,', ',', re.sub('[\n\t\r]', ' ', li.text.strip())))]
+            reference = [('x', re.sub(', *,', ',', re.sub('[\n\t\r]', ' ', div.text.strip())))]
             if rdoi:
                 reference.append(('a', 'doi:%s' % (rdoi)))
             rec['refs'].append(reference)
-    print rec
-            
-xmlf = os.path.join(xmldir,jnlfilename+'.xml')
-#xmlfile = open(xmlf, 'w')
-xmlfile  = codecs.EncodedFile(codecs.open(xmlf,mode='wb'),'utf8')
-ejlmod2.writeXML(recs ,xmlfile, publisher)
+    print '  ', rec.keys()
+
+xmlf = os.path.join(xmldir, jnlfilename+'.xml')
+xmlfile = codecs.EncodedFile(codecs.open(xmlf, mode='wb'), 'utf8')
+ejlmod2.writeXML(recs, xmlfile, publisher)
 xmlfile.close()
 
 #retrival
 retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"
 retfiles_text = open(retfiles_path,"r").read()
 line = jnlfilename+'.xml' + "\n"
-if not line in retfiles_text: 
-    retfiles = open(retfiles_path,"a")
+if not line in retfiles_text:
+    retfiles = open(retfiles_path, "a")
     retfiles.write(line)
     retfiles.close()
