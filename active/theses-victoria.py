fbfe931dfa2d2723e81a7f35c70175292429b7bd
Florian Schwennsen <florian.schwennsen@desy.de>
2020-11-26 09:26:21 +0100

    theses-victoria: produce one file instead of several

--- active/theses-victoria.py
+++ active/theses-victoria.py
@@ -27,6 +27,7 @@ stampoftoday = '%4d-%02d-%02d' % (now.year, now.month, now.day)
 publisher = 'Victoria U., Wellington'
 rpp = 50
 numofpages = 2
+jnlfilename = 'THESES-VICTORIA-%s' % (stampoftoday)
 
 
 #bad certificate
@@ -37,7 +38,6 @@ ctx.verify_mode = ssl.CERT_NONE
 hdr = {'User-Agent' : 'Magic Browser'}
 for i in range(numofpages):
     recs = []
-    jnlfilename = 'THESES-VICTORIA-%s_%02i' % (stampoftoday, i)
     tocurl = 'https://researcharchive.vuw.ac.nz/handle/10063/32/browse?rpp=%i&sort_by=2&type=dateissued&offset=%i&etal=-1&order=DESC' % (rpp, i*rpp)
     print '---{ %i/%i }---{ %s }------' % (i+1, numofpages, tocurl)
     req = urllib2.Request(tocurl, headers=hdr)
@@ -51,32 +51,32 @@ for i in range(numofpages):
             rec['tit'] = a.text.strip()
             recs.append(rec)
             
-    j = 0
-    for rec in recs:
-        j += 1
-        print '---{ %i/%i }---{ %i/%i }---{ %s }------' % (i+1, numofpages, j, len(recs), rec['artlink'])
-        req = urllib2.Request(rec['artlink'])
-        artpage = BeautifulSoup(urllib2.urlopen(req, context=ctx))
-        time.sleep(5)
-        #author
-        for meta in artpage.find_all('meta', attrs = {'name' : 'DC.creator'}):
-            rec['autaff'] = [[ meta['content'], publisher ]]
-        for meta in artpage.find_all('meta'):
-            if meta.has_attr('name'):
-                #date
-                if meta['name'] == 'DC.date':
-                    rec['date'] = meta['content'][:10]
-                #abstract
-                elif meta['name'] == 'DCTERMS.abstract':
-                    rec['abs'] = meta['content']
-                #keywords
-                elif meta['name'] == 'DC.subject':
-                    rec['keyw'].append( meta['content'] )
-                #FFT
-                elif meta['name'] == 'citation_pdf_url':
-                    rec['hidden'] = meta['content']
-                #URN
-        print '  ', rec.keys()
+j = 0
+for rec in recs:
+    j += 1
+    print '---{ %i/%i }---{ %i/%i }---{ %s }------' % (i+1, numofpages, j, len(recs), rec['artlink'])
+    req = urllib2.Request(rec['artlink'])
+    artpage = BeautifulSoup(urllib2.urlopen(req, context=ctx))
+    time.sleep(5)
+    #author
+    for meta in artpage.find_all('meta', attrs = {'name' : 'DC.creator'}):
+        rec['autaff'] = [[ meta['content'], publisher ]]
+    for meta in artpage.find_all('meta'):
+        if meta.has_attr('name'):
+            #date
+            if meta['name'] == 'DC.date':
+                rec['date'] = meta['content'][:10]
+            #abstract
+            elif meta['name'] == 'DCTERMS.abstract':
+                rec['abs'] = meta['content']
+            #keywords
+            elif meta['name'] == 'DC.subject':
+                rec['keyw'].append( meta['content'] )
+            #FFT
+            elif meta['name'] == 'citation_pdf_url':
+                rec['hidden'] = meta['content']
+            #URN
+    print '  ', rec.keys()
                 
     #closing of files and printing
     xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
