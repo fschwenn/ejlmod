a3a15781fae0acee6213a892a736893e7d7ac6bb
Florian Schwennsen <florian.schwennsen@desy.de>
2021-01-04 22:31:26 +0100

    sciencemag: added Sci.Adv.

--- active/sciencemag.py
+++ active/sciencemag.py
@@ -19,22 +19,26 @@ import os
 import datetime
 
 
-xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
-tmpdir = '/tmp'
+xmldir = '/afs/desy.de/user/l/library/inspire/ejl'#+'/special/'
+retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"#+'_special'
 
 publisher = 'American Association for the Advancement of Science'
-year = sys.argv[1]
-#vol = sys.argv[1]
-#issue = sys.argv[2]
+jnl = sys.argv[1]
+year = sys.argv[2]
 now = datetime.datetime.now()
 hdr = {'User-Agent' : 'Magic Browser'}
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-def getissuenumbers(year):
+if jnl == 'science':
+    jnlname = 'Science'
+elif jnl == 'advances':
+    jnlname = 'Sci.Adv.'
+
+def getissuenumbers(jnl, year):
     issues = []
-    tocurl = 'https://science.sciencemag.org/content/by/year/%s' % (year)
+    tocurl = 'https://%s.sciencemag.org/content/by/year/%s' % (jnl, year)
     tocreq = urllib2.Request(tocurl, headers=hdr)
     tocpage = BeautifulSoup(urllib2.urlopen(tocreq, context=ctx), features="lxml")
     for div in tocpage.find_all('div', attrs = {'class' : 'archive-issue-list'}):
@@ -46,21 +50,24 @@ def getissuenumbers(year):
     return issues
 
 def getdone():
-    regscience = re.compile('science\d+\.\d+')
+    if jnl == 'science':
+        reg = re.compile('science(\d+)\.(\d+)')
+    elif jnl == 'advances':
+        reg = re.compile('sciadv(\d+)\.(\d+)')
     ejldir = '/afs/desy.de/user/l/library/dok/ejl'
     issues = []
     for directory in [ejldir+'/onhold', ejldir+'/zu_punkten/enriched', ejldir+'/backup', ejldir+'/backup/' + str(now.year-1)]:
         for datei in os.listdir(directory):
-            if regscience.search(datei):
-                vol = re.sub('.*science(\d+).*', r'\1', datei)
-                issue = re.sub('.*science\d+\.(\d+).*', r'\1', datei)
+            if reg.search(datei):
+                regs = reg.search(datei)
+                vol = regs.group(1)
+                issue = regs.group(2)
                 issues.append((vol, issue))
     print 'done', issues
     return issues
 
-def harvestissue(vol, issue):
-    jnlname = 'Science'
-    tocurl = 'https://science.sciencemag.org/content/%s/%s' % (vol, issue)
+def harvestissue(jnl, vol, issue):
+    tocurl = 'https://%s.sciencemag.org/content/%s/%s' % (jnl, vol, issue)
     print "get table of content of %s %s.%s via %s ..." % (jnlname, vol, issue, tocurl)
     tocreq = urllib2.Request(tocurl, headers=hdr)
     tocpage = BeautifulSoup(urllib2.urlopen(tocreq, context=ctx), features="lxml")
@@ -74,7 +81,7 @@ def harvestissue(vol, issue):
             for h3 in article.find_all('h3'):
                 for a in h3.find_all('a'):
                     rec = {'tc' : 'P', 'jnl' : jnlname, 'vol' : vol, 'issue' : issue, 'year' : year, 'autaff' : []}
-                    rec['artlink'] = 'https://science.sciencemag.org' + a['href']
+                    rec['artlink'] = 'https://%s.sciencemag.org%s' % (jnl, a['href'])
                     recs.append(rec)
     #check article pages
     i = 0
@@ -82,7 +89,7 @@ def harvestissue(vol, issue):
         i += 1
         print '---{ %i/%i }---{ %s }---' % (i, len(recs), rec['artlink'])
         try:
-            time.sleep(30)
+            time.sleep(40)
             pagreq = urllib2.Request(rec['artlink'], headers=hdr)
             page = BeautifulSoup(urllib2.urlopen(pagreq), features="lxml")
         except:
@@ -134,7 +141,7 @@ def harvestissue(vol, issue):
 
 
 done = getdone()
-available = getissuenumbers(year)
+available = getissuenumbers(jnl, year)
 todo = []
 for tupel in available:
     if not tupel in done:
@@ -142,16 +149,15 @@ for tupel in available:
 i = 0
 for (vol, issue) in todo:
     i += 1
-    print '==={ %i/%i }==={ Volume %s, Issue %s }===' % (i, len(todo), vol, issue)
-    recs = harvestissue(vol, issue)
-    jnlfilename = 'science%s.%s' % (vol, issue)
+    print '==={ %i/%i }==={ %s, Volume %s, Issue %s }===' % (i, len(todo), jnlname, vol, issue)
+    recs = harvestissue(jnl, vol, issue)
+    jnlfilename = '%s%s.%s' % (re.sub('\.', '', jnlname.lower()), vol, issue)
     #write recs
     xmlf = os.path.join(xmldir, jnlfilename+'.xml')
     xmlfile = codecs.EncodedFile(codecs.open(xmlf, mode='wb'), 'utf8')
     ejlmod2.writeXML(recs, xmlfile, publisher)
     xmlfile.close()
     #retrival
-    retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"
     retfiles_text = open(retfiles_path,"r").read()
     line = jnlfilename+'.xml'+ "\n"
     if not line in retfiles_text:
