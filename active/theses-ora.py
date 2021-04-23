4cb94ae9ee2577e9f2c5d9b3cc934b47456b9b6e
Florian Schwennsen <florian.schwennsen@desy.de>
2021-04-23 09:06:01 +0200

    theses-ora: check more pages; exclude boring stuff

--- active/theses-ora.py
+++ active/theses-ora.py
@@ -16,55 +16,90 @@ import datetime
 import time
 import json
 
-xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
-retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"
+xmldir = '/afs/desy.de/user/l/library/inspire/ejl'#+'/special'
+retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"#+'_special'
 
 
 now = datetime.datetime.now()
 stampoftoday = '%4d-%02d-%02d' % (now.year, now.month, now.day)
 
+
+years = 2
+pagesdate = 1
+pagesnodate = 12
+rpp = 100
 publisher = 'Oxford University'
 
 typecode = 'T'
 
 jnlfilename = 'THESES-ORA-%s' % (stampoftoday)
 
-tocurl = 'https://ora.ox.ac.uk/search/detailed?q=%2A%3A%2A&truncate=450&filterf_subject=%22Physics%22&filterf_thesisLevel=%22Doctoral%22&rows=500&sort=timestamp%20desc'
-tocurl = 'https://ora.ox.ac.uk/?f%5Bf_degree_level%5D%5B%5D=Doctoral&f%5Bf_type_of_work%5D%5B%5D=Thesis&f%5Bf_subjects%5D%5B%5D=Physics&per_page=100&q=&search_field=all_fields&sort=publication_date+desc'
 
-try:
-    tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl))
-    time.sleep(3)
-except:
-    print "retry %s in 180 seconds" % (tocurl)
-    time.sleep(180)
-    tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl))
+nonsubjects = []
+for word in ['Africa', 'Ancient', 'Animal', 'Anthropology', 'Architecture', 'Biolog', 'Cancer',
+             'China', 'Clima', 'Law', 'history', 'Economic', 'Econometric', 'Education',
+             'literature', 'poetry', 'Therapy', 'Geolog', 'Medic', 'Psychiatr', 'Philosoph',
+             'Music', 'Oncology', 'Politic', 'Public', 'religi', 'social', 'Sociolog',
+             'Neuroscience', 'Genetics', 'Oceanic', 'disease', 'Archeology', 'Linguistics',
+             'zoology', 'Biophysics', 'Orthopaedics', 'Bioinformatics', 'Metallurgy',
+             'Aerodynamics', 'Zoological', 'Business', 'Neuroscience', 'Physiology', 'Geophysics',
+             'Physiology', 'Pharmacology', 'Metabolism', 'Agriculture', 'Anaesthetics', 'Anatomy',
+             'Antibiotics', 'Anxiety', 'linguistics', 'Arabic', 'Neuroscience', 'Biblical',
+             'Biodiversity', 'Biomimetic', 'Biosensors', 'Blood', 'Botanical', 'Buddhism',
+             'Business', 'Children', 'Chinese', 'Criminology', 'Asia', 'haematology', 'Clinical',
+             'Neuropathology', 'Settlement', 'Italian', 'Tumour', 'Gynaecology', 
+             'Surgery', 'Urban', 'Weather', 'Biochemi', 'Ophthalmology', 'Neurosciences',
+             'Theolog', 'Psychology', 'Epidemiology', 'Archaeolog', 'Chemistr', 'Neolithic',
+             'Vaccination', 'Woman', 'Immunology', 'Geography']:
+    nonsubjects.append(re.compile(word, re.IGNORECASE))
 
+tocurls = []
 prerecs = []
-#for div in tocpage.find_all('div', attrs = {'class' : 'response_doc'}):
-for div in tocpage.find_all('section', attrs = {'class' : 'document-metadata-header'}):
-    rec = {'jnl' : 'BOOK', 'tc' : typecode, 'keyw' : [], 'note' : []}
-    isnew = True
-    for li in div.find_all('li'):
-        lit = li.text.strip()
-        if re.search('^2\d\d\d', lit):
-            rec['date'] = li.text.strip()
-            year = int(re.sub('.*(20\d\d).*', r'\1', rec['date']))
-            if year < now.year - 1:
-                isnew = False
-    for h3 in div.find_all('h3'):
-        rec['tit'] = h3.text.strip()
-        for a in h3.find_all('a'):
-            rec['link'] = 'https://ora.ox.ac.uk' + a['href']
-            rec['doi'] = re.sub('.*:', '20.2000/', a['href'])
-    if isnew:
-        prerecs.append(rec)
+#include records with unknown date
+for page in range(pagesnodate):
+    tocurls.append('https://ora.ox.ac.uk/?f%5Bf_degree_level%5D%5B%5D=Doctoral&f%5Bf_type_of_work%5D%5B%5D=Thesis&page=' + str(page+1) + '&per_page=' + str(rpp) + '&range%5Bf_item_year%5D%5Bmissing%5D=true&search_field=all_fields&sort=publication_date+desc')
+#check date range
+for page in range(pagesdate):
+    tocurls.append('https://ora.ox.ac.uk/?f%5Bf_degree_level%5D%5B%5D=Doctoral&f%5Bf_type_of_work%5D%5B%5D=Thesis&page=' + str(page+1) + '&per_page=' + str(rpp) + '&search_field=all_fields&sort=publication_date+desc')
 
+i = 0
+for tocurl in tocurls:
+    i += 1
+    print '==={ %i/%i }==={ %s }===' % (i, len(tocurls), tocurl)
+    try:
+        tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl))
+        time.sleep(3)
+    except:
+        print "retry %s in 180 seconds" % (tocurl)
+        time.sleep(180)
+        tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl))
+
+    #for div in tocpage.find_all('div', attrs = {'class' : 'response_doc'}):
+    for div in tocpage.find_all('section', attrs = {'class' : 'document-metadata-header'}):
+        year = False
+        rec = {'jnl' : 'BOOK', 'tc' : typecode, 'keyw' : [], 'note' : []}
+        for li in div.find_all('li'):
+            lit = li.text.strip()
+            if re.search('^[12]\d\d\d', lit):
+                rec['date'] = li.text.strip()
+                year = int(re.sub('.*([12]\d\d\d).*', r'\1', rec['date']))
+        for h3 in div.find_all('h3'):
+            rec['tit'] = h3.text.strip()
+            for a in h3.find_all('a'):
+                rec['link'] = 'https://ora.ox.ac.uk' + a['href']
+                rec['doi'] = re.sub('.*:', '20.2000/', a['href'])
+        if year:
+            if year > now.year - years:
+                prerecs.append(rec)
+        else:
+            prerecs.append(rec)
+    print len(prerecs)
+ 
 i = 0
 recs = []
 for rec in prerecs:
     i += 1
-    print '%i/%i' % (i, len(prerecs))
+    print '---{ %i/%i (%i) }---{ %s }---' % (i, len(prerecs), len(recs), rec['link'])
     try:
         artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['link']))
         time.sleep(3)
@@ -90,10 +125,24 @@ for rec in prerecs:
             #subject
             elif meta['name'] == 'DC.subject':
                 rec['note'].append(meta['content'])
+    #license
+    for a in artpage.body.find_all('a'):
+        if a.has_attr('href') and re.search('creativecommons.org', a['href']):
+            rec['license'] = {'url' : a['href']}
     #bad metadata
     if not rec.has_key('auts'):
         rec['auts'] = [ 'Mustermann, Martin' ]
-    recs.append(rec)
+    interesting = True
+    for note in rec['note']:
+        if interesting:
+            for word in nonsubjects:
+                if word.search(note):
+                    print '  skip ', note
+                    interesting = False
+                    break
+    if interesting:
+        print ' ', rec.keys()
+        recs.append(rec)
 
     
 #closing of files and printing
