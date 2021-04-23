f1fab95593db059f645680832cccc9acd65d98ac
Florian Schwennsen <florian.schwennsen@desy.de>
2021-04-23 09:01:31 +0200

    theses-edinburgh: harvest several pages

--- active/theses-edinburgh.py
+++ active/theses-edinburgh.py
@@ -2,7 +2,6 @@
 #harvest theses from University of Edinburgh
 #FS: 2019-11-25
 
-
 import getopt
 import sys
 import os
@@ -16,38 +15,39 @@ import datetime
 import time
 import json
 
-xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
-retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"
-
+xmldir = '/afs/desy.de/user/l/library/inspire/ejl'#+'/special'
+retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"#+'_special'
 
 now = datetime.datetime.now()
 stampoftoday = '%4d-%02d-%02d' % (now.year, now.month, now.day)
 
 publisher = 'Edinburgh U.'
-
-typecode = 'T'
+rpp = 50
+pages = 1
 
 jnlfilename = 'THESES-EDINBURGH-%s' % (stampoftoday)
 hdr = {'User-Agent' : 'Magic Browser'}
-tocurl = 'https://www.era.lib.ed.ac.uk/handle/1842/3420/discover?sort_by=dc.date.issued_dt&order=desc&rpp=50'
-print tocurl
-req = urllib2.Request(tocurl, headers=hdr)
-tocpage = BeautifulSoup(urllib2.urlopen(req))
 recs = []
 hdls = []
-for div in tocpage.body.find_all('div', attrs = {'class' : 'artifact-description'}):
-    rec = {'tc' : 'T', 'keyw' : [], 'jnl' : 'BOOK'}
-    for a in div.find_all('a'):
-        rec['artlink'] = 'https://www.era.lib.ed.ac.uk' + a['href'] #+ '?show=full'
-        rec['hdl'] = re.sub('.*handle\/', '', a['href'])
-        if not rec['hdl'] in hdls:
-            recs.append(rec)
-            hdls.append(rec['hdl'])
+for page in range(pages):
+    tocurl = 'https://www.era.lib.ed.ac.uk/handle/1842/3420/discover?sort_by=dc.date.issued_dt&order=desc&rpp=' + str(rpp) + '&page=' + str(page+1)
+    print '==={ %i/%i }==={ %s }===' % (page+1, pages, tocurl)
+    req = urllib2.Request(tocurl, headers=hdr)
+    tocpage = BeautifulSoup(urllib2.urlopen(req))
+    for div in tocpage.body.find_all('div', attrs = {'class' : 'artifact-description'}):
+        rec = {'tc' : 'T', 'keyw' : [], 'jnl' : 'BOOK'}
+        for a in div.find_all('a'):
+            rec['artlink'] = 'https://www.era.lib.ed.ac.uk' + a['href'] #+ '?show=full'
+            rec['hdl'] = re.sub('.*handle\/', '', a['href'])
+            if not rec['hdl'] in hdls:
+                recs.append(rec)
+                hdls.append(rec['hdl'])
+    time.sleep(5)
 
 i = 0
 for rec in recs:
     i += 1
-    print '---{ %i/%i}---{ %s }------' % (i, len(recs), rec['artlink'])
+    print '---{ %i/%i }---{ %s }------' % (i, len(recs), rec['artlink'])
     try:
         artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
         time.sleep(3)
