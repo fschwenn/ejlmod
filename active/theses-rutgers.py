9d2d8f1f93e7c137dc8e1b6247b774071c3df2e7
Florian Schwennsen <florian.schwennsen@desy.de>
2021-07-23 15:58:51 +0200

    theses-rutgers: add xml-filename to metadata

--- active/theses-rutgers.py
+++ active/theses-rutgers.py
@@ -108,7 +108,7 @@ for rec in recs:
 #closing of files and printing
 xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
 xmlfile  = codecs.EncodedFile(codecs.open(xmlf,mode='wb'),'utf8')
-ejlmod2.writeXML(recs, xmlfile, publisher)
+ejlmod2.writenewXML(recs, xmlfile, publisher, jnlfilename)
 xmlfile.close()
 #retrival
 retfiles_text = open(retfiles_path,"r").read()
@@ -127,7 +127,7 @@ if not line in retfiles_text:
 #    #closing of files and printing
 #    xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
 #    xmlfile  = codecs.EncodedFile(codecs.open(xmlf,mode='wb'),'utf8')
-#    ejlmod2.writeXML(recsdict[program], xmlfile, publisher)
+#    ejlmod2.writenewXML(recsdict[program], xmlfile, publisher, jnlfilename)
 #    xmlfile.close()
 #    #retrival
 #    retfiles_text = open(retfiles_path,"r").read()
