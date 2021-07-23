eed437e8aa50dad3c4c0142a4ac1d0b0e347e244
Florian Schwennsen <florian.schwennsen@desy.de>
2021-07-23 15:58:01 +0200

    theses-narcis: add xml-filename to metadata

--- active/theses-narcis.py
+++ active/theses-narcis.py
@@ -213,7 +213,7 @@ if len(recs) < 200:
     #closing of files and printing
     xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
     xmlfile  = codecs.EncodedFile(codecs.open(xmlf,mode='wb'),'utf8')
-    ejlmod2.writeXML(recs, xmlfile, publisher)
+    ejlmod2.writenewXML(recs, xmlfile, publisher, jnlfilename)
     xmlfile.close()
     #retrival
     retfiles_text = open(retfiles_path,"r").read()
@@ -230,7 +230,7 @@ else:
         #closing of files and printing
         xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
         xmlfile  = codecs.EncodedFile(codecs.open(xmlf,mode='wb'),'utf8')
-        ejlmod2.writeXML(dictrecs[unikey], xmlfile, publisher)
+        ejlmod2.writenewXML(dictrecs[unikey], xmlfile, publisher, jnlfilename)
         xmlfile.close()
         #retrival
         retfiles_text = open(retfiles_path,"r").read()
