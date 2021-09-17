ed52154b15effbb268cd262b5f60bfad78299743
Florian Schwennsen <florian.schwennsen@desy.de>
2021-09-17 11:12:40 +0200

    theses-toronto: added Fieldcodes and boring Department

--- active/theses-toronto.py
+++ active/theses-toronto.py
@@ -24,12 +24,36 @@ now = datetime.datetime.now()
 stampoftoday = '%4d-%02d-%02d' % (now.year, now.month, now.day)
 
 publisher = 'Toronto U.'
+boringdeps = ['Adult Education and Counselling Psychology', 'Aerospace Science and Engineering',
+              'Anthropology', 'Applied Psychology and Human Development', 'Biochemistry',
+              'Biomedical Engineering', 'Cell and Systems Biology', 'Cinema Studies',
+              'Civil Engineering', 'Classics', 'Comparative Literature', 'Criminology',
+              'Curriculum, Teaching and Learning', 'Dalla Lana School of Public Health',
+              'Dentistry', 'Drama', 'Earth Sciences', 'East Asian Studies', 'English',
+              'Exercise Sciences', 'French Language and Literature', 'Geography', 'Law',
+              'Leadership, Higher and Adult Education', 'Linguistics', 'Management',
+              'Materials Science and Engineering', 'Mechanical and Industrial Engineering', 
+              'Health Policy, Management and Evaluation', 'Immunology', 'Medical Biophysics', 
+              'Industrial Relations and Human Resources', 'Information Studies', 'Medical Science',
+              'Medieval Studies', 'Molecular and Medical Genetics', 'Molecular Genetics', 'Music',
+              'Near and Middle Eastern Civilizations', 'Nursing Science', 'Nutritional Sciences',
+              'Pharmaceutical Sciences', 'Pharmacology', 'Philosophy', 'Religion, Study of',
+              'Social Justice Education', 'Social Work', 'Sociology', 'Spanish', 'Statistics', 
+              'Psychological Clinical Science', 'Psychology', 'Rehabilitation Science', 
+              'Physical and Environmental Sciences', 'Physiology', 'Political Science',
+              'Architecture, Landscape, and Design', 'Forestry', 'Human Development and Applied Psychology',
+              'Kinesiology and Physical Education', 'Laboratory Medicine and Pathobiology', 
+              'History and Philosophy of Science and Technology', 'History of Art', 'History', 
+              'Ecology and Evolutionary Biology', 'Economics', 'Electrical and Computer Engineering', 
+              'Chemical Engineering Applied Chemistry', 'Chemistry', 'Women and Gender Studies Institute',
+              'Chemical Engineering Applied Chemistry', 'Germanic Languages and Literatures',
+              'Sociology and Equity Studies in Education', 'Speech-Language Pathology']
 
 rpp = 50
 pages = 10 
 
 hdr = {'User-Agent' : 'Magic Browser'}
-recs = []
+prerecs = []
 deprecs = {}
 for i in range(pages):
     tocurl = 'https://tspace.library.utoronto.ca/handle/1807/9945/browse?rpp=' + str(rpp) + '&sort_by=2&type=dateissued&offset=' + str(i*rpp) + '&etal=-1&order=DESC'
@@ -42,13 +66,15 @@ for i in range(pages):
             for a in td.find_all('a'):
                 rec['artlink'] = 'https://tspace.library.utoronto.ca' + a['href'] #+ '?show=full'
                 rec['hdl'] = re.sub('.*handle\/', '', a['href'])
-                recs.append(rec)
+                prerecs.append(rec)
     time.sleep(15)
 
 i = 0
-for rec in recs:
+recs = []
+for rec in prerecs:
     i += 1
-    print '---{ %i/%i }---{ %s }------' % (i, len(recs), rec['artlink'])
+    keepit = True
+    print '---{ %i/%i (%i) }---{ %s }------' % (i, len(prerecs), len(recs), rec['artlink'])
     try:
         artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
         time.sleep(3)
@@ -105,21 +131,28 @@ for rec in recs:
                         rec['supervisor'].append([at, publisher])
                     #department
                     elif spant == 'Department:':
+                        if at == 'Astronomy and Astrophysics':
+                            rec['fc'] = 'a'
+                        elif at == 'Mathematics':
+                            rec['fc'] = 'm'
                         rec['note'].append(at)
                         if at in deprecs.keys():
                             deprecs[at].append(rec)
                         else:
                             deprecs[at] = [rec]
-        #
+                        if at in boringdeps:
+                            keepit = False
+        #if i % 10 == 0:
+        #    print ', '.join(['%s (%s)' % (dep, len(deprecs[dep])) for dep in deprecs.keys()])
+    if keepit:
         print '  ', rec.keys()
-        if i % 10 == 0:
-            print ', '.join(['%s (%s)' % (dep, len(deprecs[dep])) for dep in deprecs.keys()])
+        recs.append(rec)
 
 jnlfilename = 'THESES-TORONTO-%s' % (stampoftoday)
 #closing of files and printing
 xmlf = os.path.join(xmldir,jnlfilename+'.xml')
 xmlfile  = codecs.EncodedFile(codecs.open(xmlf,mode='wb'),'utf8')
-ejlmod2.writenewXML(deprecs[dep], xmlfile, publisher, jnlfilename)
+ejlmod2.writenewXML(recs, xmlfile, publisher, jnlfilename)
 xmlfile.close()
 #retrival
 retfiles_text = open(retfiles_path,"r").read()
