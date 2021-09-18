747485c2cebf66b59e6e650109054fb98d82f3b2
Florian Schwennsen <florian.schwennsen@desy.de>
2021-09-18 10:27:05 +0200

    theses-cologne: bug fix: stupid typo from field code adding

--- active/theses-cologne.py
+++ active/theses-cologne.py
@@ -156,7 +156,7 @@ for rec in prerecs:
         if meta['content'] in divisionsdict.keys():
             if meta['content'] in ['inst_50095', 'inst_50135', 'klips-14725', 'klips-14795']:
                 rec['fc'] = 'm'
-            elif in ['klips-14751']:
+            elif meta['content'] in ['klips-14751']:
                 rec['fc'] = 'c'            
             rec['autaff'][-1].append(divisionsdict[ meta['content'] ])
         else:
