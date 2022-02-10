ce3a37db7b0d6c600ff2c46b701648fac26ce332
Florian Schwennsen <florian.schwennsen@desy.de>
2022-02-10 11:31:28 +0100

    armenianjp: new toc URL, added DOI, keywords

--- active/armenianjp.py
+++ active/armenianjp.py
@@ -35,6 +35,9 @@ issn = "1829-1171"
 urltrunk = 'http://www.flib.sci.am/eng/journal/Phys'
 urltrunk = 'https://www.flib.sci.am/journal/arm/Phys'
 tocurl = '%s/PV%sIss%s.html' % (urltrunk, vol, issue)
+tocurl = '%s/%i_%s.html' % (urltrunk, int(vol)+2007, issue)
+
+
 
 #bad certificate
 ctx = ssl.create_default_context()
@@ -44,7 +47,7 @@ ctx.verify_mode = ssl.CERT_NONE
 hdr = {'User-Agent' : 'Magic Browser'}
 print "get table of content of %s %s,  No. %s via %s ..." % (jnlname, vol, issue, tocurl)
 req = urllib2.Request(tocurl, headers=hdr)
-tocpage = BeautifulSoup(urllib2.urlopen(req, context=ctx))
+tocpage = BeautifulSoup(urllib2.urlopen(req, context=ctx), features="lxml")
 time.sleep(1)
 recs = []
 for tr in tocpage.body.find_all('tr'):
@@ -52,16 +55,15 @@ for tr in tocpage.body.find_all('tr'):
            'issue' : issue, 'jnl' : jnlname, 'note' : [], 'tc' : "P"}
     for a in tr.find_all('a'):
         if a.has_attr('href'):
-            rec['link'] = a['href']
-            rec['doi'] = '20.2000/' + re.sub('\W', '', a['href'][7:])
+            rec['artlink'] = a['href']
             recs.append(rec)
 
 i = 0
 for rec in recs:
     i += 1
-    print '---{ %i/%i }---{ %s }------' % (i, len(recs), rec['link'])
-    req = urllib2.Request(rec['link'])
-    artpage = BeautifulSoup(urllib2.urlopen(req, context=ctx))
+    print '---{ %i/%i }---{ %s }------' % (i, len(recs), rec['artlink'])
+    req = urllib2.Request(rec['artlink'])
+    artpage = BeautifulSoup(urllib2.urlopen(req, context=ctx), features="lxml")
     time.sleep(3)
     #license
     for a in artpage.find_all('a'):
@@ -92,10 +94,25 @@ for rec in recs:
             elif meta['name'] == ['eprints.document_url', 'citation_pdf_url']:
                 if 'license' in rec.keys():
                     rec['FFT'] = meta['content']
-    print ' ', rec.keys()                 
+            #DOI
+            elif meta['name'] == 'DC.identifier':
+                if re.search('doi.org', meta['content']):
+                    rec['doi'] = re.sub('.*.org\/', '', meta['content'])
+                elif re.search('pdf$', meta['content']):  
+                    if 'license' in rec.keys():
+                        rec['FFT'] = meta['content']
+                    else:
+                        rec['hidden'] = meta['content']
+    #keywords
+    for a in  artpage.body.find_all('a', attrs = {'class' : 'object__keyword'}):
+        rec['keyw'].append(a.text.strip())
+    if not 'doi' in rec.keys():
+        rec['link'] = rec['artlink']
+        rec['doi'] = '20.2000/ArmenianJPhysics/' + re.sub('\D', '', rec['artlink'])
+    print ' ', rec.keys()
 
-xmlf    = os.path.join(xmldir,jnlfilename+'.xml')
-xmlfile  = open(xmlf,'w')
+xmlf  = os.path.join(xmldir,jnlfilename+'.xml')
+xmlfile = open(xmlf,'w')
 ejlmod2.writenewXML(recs,xmlfile,publisher, jnlfilename)
 xmlfile.close()
 
