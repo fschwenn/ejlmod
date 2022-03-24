2a2112fdec0fbf40138ecd524fb5e3e28c42ef25
Florian Schwennsen <florian.schwennsen@desy.de>
2022-03-24 11:58:09 +0100

    theses-iowa: major rewrite due to webpage change

--- active/theses-iowa.py
+++ active/theses-iowa.py
@@ -13,6 +13,7 @@ import ejlmod2
 import codecs
 import datetime
 import time
+import json
 
 xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
 retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"
@@ -23,96 +24,91 @@ stampoftoday = '%4d-%02d-%02d' % (now.year, now.month, now.day)
 publisher = 'Iowa State U. (main)'
 hdr = {'User-Agent' : 'Magic Browser'}
 
-prerecs = []
+rpp = 10
+pages = 1
+recs = []
 jnlfilename = 'THESES-IOWASTATE-%s' % (stampoftoday)
 
-tocurl = 'https://lib.dr.iastate.edu/physastro_etd/'
-print tocurl
-req = urllib2.Request(tocurl, headers=hdr)
-tocpage = BeautifulSoup(urllib2.urlopen(req))
-time.sleep(2)
-for div in tocpage.body.find_all('div', attrs = {'id' : 'series-home'}):
-    year = False
-    for child in div.children:
-        try:
-            child.name
-        except:
-            continue
-        if child.name == 'h4':
-            for span in child.find_all('span'):
-                year = int(span.text.strip())
-                print '==={ %i }===' % (year)
-        elif child.name == 'p' and year:
-            if year > now.year - 2:
-                for a in child.find_all('a'):
-                    if a.has_attr('href') and re.search('\d$', a['href']):
-                        rec = {'tc' : 'T', 'jnl' : 'BOOK', 'link' : a['href'], 'note' : [],
-                               'year' : str(year), 'supervisor' : []}
-                        rec['tit'] = a.text.strip()
-                        rec['doi'] = '20.2000/IowaStateU/' + re.sub('.*\/', '', a['href'])
-                        prerecs.append(rec)
-            else:
-                break
+boringdegrees = ['Master of Science']
+
+for (fc, dep) in [('', 'Physics%20and%20Astronomy'), ('m', 'Mathematics')]:
+    for page in range(pages):
+        print '==={ %i/%i %s }===' % (page+1, pages, dep)
+        tocurl = 'https://dr.lib.iastate.edu/collections/0830d32e-14e1-4a4f-bb8f-271a75ed35af?scope=0830d32e-14e1-4a4f-bb8f-271a75ed35af&view=listElement&f.department=' + dep + ',equals&spc.sf=dc.date.issued&spc.sd=DESC&spc.rpp=' + str(rpp) + '&spc.page=' + str(page+1)
+        print '  ', tocurl
+        req = urllib2.Request(tocurl, headers=hdr)
+        tocpage = BeautifulSoup(urllib2.urlopen(req), features="lxml")
+        for script in tocpage.find_all('script', attrs = {'id' : 'dspace-angular-state'}):
+            scriptt = re.sub('&q;', '"', re.sub('[\n\t]', '', script.contents[0].strip()))
+            scripttjson = json.loads(scriptt)
+            for k in scripttjson['NGRX_STATE']['core']['cache/object'].keys():
+                keepit = True
+                rec = {'tc' : 'T', 'jnl' : 'BOOK', 'note' : [], 'supervisor' : []}
+                if fc: rec['fc'] = fc
+                if 'metadata' in scripttjson['NGRX_STATE']['core']['cache/object'][k]['data'].keys():
+                    metadata = scripttjson['NGRX_STATE']['core']['cache/object'][k]['data']['metadata']
+                    #print 'K', metadata.keys()
+                    for tag in metadata.keys():
+                        #degree
+                        if tag == 'thesis.degree.name':
+                            degree = metadata[tag][0]['value']
+                            if degree in boringdegrees:
+                                keepit = False
+                            else:
+                                rec['note'].append(degree)
+                        elif tag == 'dc.type.genre':
+                            rec['note'].append(metadata[tag][0]['value'])
+                        #date
+                        elif tag == 'dc.date.issued':
+                            rec['date'] = metadata[tag][0]['value']
+                        #title
+                        elif tag == 'dc.title':
+                            rec['tit'] = metadata[tag][0]['value']
+                        #supervisor
+                        elif tag == 'dc.contributor.advisor':
+                            for value in metadata[tag]:
+                                rec['supervisor'].append([value['value']])
+                        #embargo
+                        elif tag == 'dc.date.embargo':
+                            rec['embargo'] = (metadata[tag][0]['value'] > stampoftoday)
+                        #DOI
+                        elif tag == 'dc.identifier.doi':
+                            rec['doi'] = re.sub('.*doi.org\/', '', metadata[tag][0]['value'])
+                        #keywords
+                        elif tag == 'dc.subject':
+                            rec['keyw'] = []
+                            for value in metadata[tag]:
+                                rec['keyw'].append(value['value'])
+                        #abstract
+                        elif tag == 'dc.description.abstract':
+                            rec['abs'] = re.sub('&l;.?p&g;', '', metadata[tag][0]['value'])
+                        #author
+                        elif tag == 'dc.contributor.author':
+                            rec['autaff'] = [[ metadata[tag][0]['value'], publisher ]]
+                        #link
+                        elif tag == 'dc.identifier.uri':
+                            rec['artlink'] = metadata[tag][0]['value']
+                            if re.search('handle\/20.500.12876', rec['artlink']):
+                                rec['hdl'] = re.sub('.*handle\/', '', rec['artlink'])
+                            if not 'dc.identifier.doi' in metadata.keys():
+                                rec['link'] = rec['artlink']
+                    if keepit and 'autaff' in rec.keys():
+                        recs.append(rec)
+            time.sleep(5)
+
+print 'get fulltext links'
 
 i = 0
-recs = []
-for rec in prerecs:
-    keepit = True
+for rec in recs:
     i += 1
-    print '---{ %i/%i (%i) }---{ %s }------' % (i, len(prerecs), len(recs), rec['link'])
-    try:
-        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['link']))
-        time.sleep(10)
-    except:
-        try:
-            print 'retry %s in 180 seconds' % (rec['link'])
-            time.sleep(180)
-            artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['link']))
-        except:
-            print 'no access to %s' % (rec['link'])
-            continue
-    for meta in artpage.head.find_all('meta'):
-        if meta.has_attr('name'):
-            #author
-            if meta['name'] == 'author':
-                rec['autaff'] = [[ meta['content'] ]]
-            #keywords
-            elif meta['name'] == 'keywords':
-                rec['keyw'] = re.split(', ', meta['content'])
-            #abstract
-            elif meta['name'] == 'description':
-                rec['abs'] = meta['content']
-            #date
-            elif meta['name'] == 'bepress_citation_date':
-                rec['date'] = meta['content']
-            #PDF
-            elif meta['name'] == 'bepress_citation_pdf_url':
-                rec['hidden'] = meta['content']
-            #degree
-            elif meta['name'] == 'bepress_citation_dissertation_name':
-                if meta['content'] in ['Master of Science']:
-                    keepit = False
-                else:
-                    rec['note'].append(meta['content'])
-    rec['autaff'][-1].append(publisher)
-    #subject
-    for div in artpage.body.find_all('id', attrs = {'id' : 'major'}):
-        for p in div.find_all('p'):
-            rec['note'].append(p.text.strip())
-    #supervisor
-    for div in artpage.body.find_all('id', attrs = {'id' : 'advisor1'}):
-        for p in div.find_all('p'):
-            rec['supervisor'].append([p.text.strip()])
-    for div in artpage.body.find_all('id', attrs = {'id' : 'advisor2'}):
-        for p in div.find_all('p'):
-            rec['supervisor'].append([p.text.strip()])
-    #pages
-    for div in artpage.body.find_all('id', attrs = {'id' : 'file_size'}):
-        for p in div.find_all('p'):
-            rec['pages'] = re.sub('\D', '', p.text.strip())
-    if keepit:
-        print '  ', rec.keys()
-        recs.append(rec)
+    print '---{ %i/%i }---{ %s }---' % (i, len(recs), rec['artlink'])
+    req = urllib2.Request(rec['artlink'], headers=hdr)
+    artpage = BeautifulSoup(urllib2.urlopen(req), features="lxml")
+    for meta in artpage.head.find_all('meta', attrs = {'property' : 'citation_pdf_url'}):
+        rec['hidden'] = 'https://dr.lib.iastate.edu' + meta['content']
+    time.sleep(5)
+    print '    ', rec.keys()
+            
 
 #closing of files and printing
 xmlf = os.path.join(xmldir, jnlfilename+'.xml')
