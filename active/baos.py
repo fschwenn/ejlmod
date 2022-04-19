73664f2b93eb3d64947a008f968c36ad1a2ba1e4
Florian Schwennsen <florian.schwennsen@desy.de>
2022-04-19 16:18:33 +0200

    baos: completely rewritten

--- active/baos.py
+++ active/baos.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 #program to harvest Bulgarian Academy of Science
-# FS 2018-09-24
+# FS 2022-04-14
 
 import sys
 import os
@@ -15,95 +15,103 @@ import time
 
 xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
 retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"
+tmpdir = '/tmp'
+
+def tfstrip(x): return x.strip()
+regexpref = re.compile('[\n\r\t]')
+
+typecode = 'P'
+
+issueid = sys.argv[1]
 
 publisher = 'Bulgarian Academy of Sciences'
-typecode = 'P'
-year = sys.argv[1]
-issue = sys.argv[2]
-jnlfilename = 'crabs%s.%s' % (year, issue)
-
-if issue == '10':
-    urltrunk = 'http://www.proceedings.bas.bg/content/%s_a_cntent.html' % (year)
-elif issue == '11':
-    urltrunk = 'http://www.proceedings.bas.bg/content/%s_b_cntent.html' % (year)
-elif issue == '12':
-    urltrunk = 'http://www.proceedings.bas.bg/content/%s_c_cntent.html' % (year)
-else:
-    urltrunk = 'http://www.proceedings.bas.bg/content/%s_%s_cntent.html' % (year, issue)
+urltrunk = 'http://www.proceedings.bas.bg/index.php/cr/issue/view/%s' % (issueid)
+    
 print urltrunk
 try:
-    tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(urltrunk))
+    tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(urltrunk), features="lxml")
     time.sleep(3)
 except:
     print "retry %s in 180 seconds" % (urltrunk)
     time.sleep(180)
-    tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(urltrunk))
+    tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(urltrunk), features="lxml")
 
 recs = []
-for tr in tocpage.body.find_all('tr'):
-    for a in tr.find_all('a', attrs = {'title' : 'Click For Abstract'}):
-        artlink = a['onclick']
-        artlink = re.sub('.*(http.*)\'', r'\1', artlink)
-        try:
-            print artlink
-            artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(artlink))
-            time.sleep(3)
-        except:
-            print "retry %s in 180 seconds" % (artlink)
-            time.sleep(180)
-            artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(artlink))
-        rec = {'jnl' : 'Compt.Rend.Acad.Bulg.Sci.', 'issue' : issue, 'tc' : typecode,
-               'year' : year}
-        #pubnote
-        for center in artpage.body.find_all('center'):
-            for small in center.find_all('small'):
-                st = small.text.strip()
-                if re.search('Vol \d+', st):
-                    rec['vol'] = re.sub('.*Vol (\d+).*', r'\1', st)
-                p1p2 = re.sub('.*pp\.? *(\d.*\d).*', r'\1', st)
-                parts = re.split('\-', p1p2)
-                rec['p1'] = parts[0]
-                if len(parts) > 1:
-                    rec['p2'] = parts[1]
-        #title
-        for h2 in artpage.body.find_all('h2'):
-            if not 'tit' in rec.keys():
-                rec['tit'] = h2.text.strip()
-        #authors
-        for h3 in artpage.body.find_all('h3'):
-            rec['auts'] = re.split(' *, *', h3.text.strip())
-        #abstract
-        for p in artpage.body.find_all('p', attrs = {'class' : 'abs'}):
-            rec['abs'] = p.text.strip()
-        #keywords
-        for p in artpage.body.find_all('p', attrs = {'class' : 'keyw'}):
-            keyw = re.sub('Key words: *', '', p.text.strip())
-            rec['keyw'] = re.split(' *, *', keyw)
-        #doi
-        for div in artpage.body.find_all('div'):
-            for small in div.find_all('small'):
-                st = small.text.strip()
-                if re.search('DOI', st):
-                    rec['doi'] = re.sub('DOI: *', '', st)
-        #topic               
-        for p in artpage.body.find_all('p', attrs = {'class' : 'right'}):
-            rec['note'] = [ p.text.strip() ]
-        #artiocle link
-        if not 'doi' in rec.keys():
-            rec['link'] = artlink
-        recs.append(rec)
+for div in tocpage.body.find_all('div', attrs = {'class' : 'section'}):
+    for h2 in div.find_all('h2'):
+        note = regexpref.sub('', h2.text).strip()
+        print ' - %s - ' % (note)
+    for h3 in div.find_all('h3'):
+        rec = {'jnl' : 'Compt.Rend.Acad.Bulg.Sci.', 'tc' : 'P', 'note' : [note], 'keyw' : [],
+               'autaff' : []}
+        if note in ['Mathematics']:
+            rec['fc'] = 'm'
+        elif note in ['Chemistry', 'Biology', 'Medicine', 'Geophysics', 'Agricultural Sciences'
+                      'Engineering Sciences', 'Geology']:
+            rec['fc'] = 'o'
+        elif note in ['Space Sciences']:
+            rec['fc'] = 'a'
+        for a in h3.find_all('a'):
+            rec['artlink'] = a['href']
+            recs.append(rec)
 
-                                      
+i = 0
+for rec in recs:
+    i += 1
+    autaff = False
+    time.sleep(3)
+    print '---{ %i/%i }---{ %s }---' % (i, len(recs), rec['artlink'])
+    artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
+    for meta in artpage.head.find_all('meta'):
+        if meta.has_attr('name'):
+            #date
+            if meta['name'] == 'citation_date':
+                rec['date'] = meta['content']
+            #title
+            elif meta['name'] == 'citation_title':
+                rec['tit'] = meta['content']
+            #article ID
+            elif meta['name'] == 'citation_firstpage':
+                rec['p1'] = meta['content']
+            #keywords
+            elif meta['name'] == 'citation_keywords':
+                rec['keyw'].append(meta['content'])
+            #DOI
+            elif meta['name'] == 'citation_doi':
+                rec['doi'] = meta['content']
+            #authors and affiliations
+            elif meta['name'] == 'citation_author':
+                rec['autaff'].append([ meta['content'] ])
+            elif meta['name'] == 'citation_author_institution':
+                rec['autaff'][-1].append(meta['content'])
+            #volume and issue
+            elif meta['name'] == 'citation_issue':
+                    rec['issue'] = meta['content']
+            elif meta['name'] == 'citation_volume':
+                    rec['vol'] = meta['content']
+            #abstract
+            elif meta['name'] == 'DC.Description':
+                rec['abs'] = meta['content']
+            #fulltext
+            elif meta['name'] == 'citation_pdf_url':
+                rec['hidden'] = meta['content']
+                            
+            #references
+            elif meta['name'] == 'citation_reference':
+                rec['refs'].append([('x', meta['content'])])
+    print [(k, len(rec[k])) for k in rec.keys()]
+
+jnlfilename = 'crabs%s.%s_%s' % (rec['vol'], rec['issue'], issueid)
 #closing of files and printing
-xmlf    = os.path.join(xmldir ,jnlfilename+'.xml')
-xmlfile  = codecs.EncodedFile(codecs.open(xmlf, mode='wb'), 'utf8')
+xmlf = os.path.join(xmldir, jnlfilename+'.xml')
+xmlfile = codecs.EncodedFile(codecs.open(xmlf, mode='wb'), 'utf8')
 ejlmod2.writenewXML(recs, xmlfile, publisher, jnlfilename)
 xmlfile.close()
 #retrival
 retfiles_text = open(retfiles_path, "r").read()
 line = jnlfilename+'.xml'+ "\n"
-if not line in retfiles_text:
+if not line in retfiles_text: 
     retfiles = open(retfiles_path, "a")
     retfiles.write(line)
     retfiles.close()
-
+ 
