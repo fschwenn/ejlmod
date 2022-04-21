6305e05a898ccd12f8d56b9a76b5dcb0292a860b
Florian Schwennsen <florian.schwennsen@desy.de>
2022-04-21 13:04:05 +0200

    jhap: new code to harvest JHAP

---
+++ active/jhap.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# program to harvest Journal of Holography Applications in Physics (JHAP)
+
+import sys
+import os
+import ejlmod2
+import re
+import urllib2
+import urlparse
+import codecs
+from bs4 import BeautifulSoup
+import datetime
+import time
+
+xmldir = '/afs/desy.de/user/l/library/inspire/ejl'
+retfiles_path = "/afs/desy.de/user/l/library/proc/retinspire/retfiles"
+now = datetime.datetime.now()
+stampoftoday = '%4d-%02d-%02d' % (now.year, now.month, now.day)
+
+publisher = 'Damghan University'
+tocurl = sys.argv[1]
+
+
+try:
+    tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl))
+    time.sleep(3)
+except:
+    print "retry %s in 180 seconds" % (tocurl)
+    time.sleep(180)
+    tocpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(tocurl))
+
+recs = []
+for h5 in tocpage.body.find_all('h5'):
+    for a in h5.find_all('a', attrs = {'class' : 'tag_a'}):
+        rec = {'jnl' : 'JHAP', 'tc' : 'P', 'autaff' : [], 'keyw' : []}
+        rec['artlink'] = 'http://jhap.du.ac.ir/' + a['href']
+        recs.append(rec)
+
+i = 0
+for rec in recs:
+    i += 1
+    print '---{ %i/%i }---{ %s }---' % (i, len(recs), rec['artlink'])
+    try:
+        time.sleep(3)
+        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))
+    except:
+        print "retry %s in 180 seconds" % (rec['artlink'])
+        time.sleep(180)
+        artpage = BeautifulSoup(urllib2.build_opener(urllib2.HTTPCookieProcessor).open(rec['artlink']))    
+    for meta in artpage.head.find_all('meta'):
+        if meta.has_attr('name'):
+            #author
+            if meta['name'] == 'citation_author':
+                rec['autaff'].append([meta['content']])
+            #affiliation
+            elif meta['name'] == 'citation_author_institution':
+                rec['autaff'][-1].append(meta['content'])
+            #date
+            elif meta['name'] == 'citation_date':
+                rec['date'] = meta['content']
+            #date
+            elif meta['name'] == 'citation_doi':
+                rec['doi'] = meta['content']
+            #pbn
+            elif meta['name'] == 'citation_volume':
+                rec['vol'] = meta['content']
+            elif meta['name'] == 'citation_issue':
+                rec['issue'] = meta['content']
+            elif meta['name'] == 'citation_firstpage':
+                rec['p1'] = meta['content']
+            elif meta['name'] == 'citation_lastpage':
+                rec['p2'] = meta['content']
+            #FFT
+            elif meta['name'] == 'citation_pdf_url':
+                rec['FFT'] = meta['content']
+            #abstract
+            elif meta['name'] == 'citation_abstract':
+                rec['abs'] = meta['content']
+            #keywords
+            elif meta['name'] == 'keywords':
+                if meta['content'] != 'Journal of Holography Applications in Physics,JHAP':
+                    rec['keyw'] = re.split(',', meta['content'])
+    print '  ', rec.keys()
+
+
+                
+jnlfilename = 'jhap%s.%s_%s' % (rec['vol'], rec['issue'], stampoftoday)
+#closing of files and printing
+xmlf = os.path.join(xmldir, jnlfilename+'.xml')
+xmlfile = codecs.EncodedFile(codecs.open(xmlf, mode='wb'), 'utf8')
+ejlmod2.writenewXML(recs, xmlfile, publisher, jnlfilename)
+xmlfile.close()
+#retrival
+retfiles_text = open(retfiles_path, "r").read()
+line = jnlfilename+'.xml'+ "\n"
+if not line in retfiles_text: 
+    retfiles = open(retfiles_path, "a")
+    retfiles.write(line)
+    retfiles.close()
+ 
